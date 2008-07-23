31e7190b2442f5f7401b2b12909769d265904f04
David Cok <davidcok@users.noreply.github.com>
2008-07-23 22:02:45 +0000

    Continuing the last commit with the unversioned files that are not obviously omitted in the new subclipse UI.
    git-svn-id: file:///home/jls/openjml/repo_master/OpenJML/trunk@1088 1627bae4-3225-46cb-a19f-684862636fd9

---
+++ OpenJML/tests/tests/misctests.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.jmlspecs.models.JMLByte;
+
+import junit.framework.TestCase;
+
+public class misctests extends TestCase {
+
+    public void test1() {
+        JMLByte b = new JMLByte((byte)1);
+    }
+    
+    public void test2() {
+        JMLByte b = new JMLByte((byte)1);
+        Object bb = b.clone();
+    }
+    
+}
