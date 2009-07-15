8ee6381046c0ff1924d35d7acacf6ec42c63054d
mcimadamore <mcimadamore@localhost>
2009-07-15 10:25:01 +0100

    6846972: cannot access member of raw type when erasure change overriding into overloading Summary: fix of 6400189 caused a nasty problem in method resolution Reviewed-by: jjg

---
+++ src/share/classes/com/sun/tools/javac/file/SymbolArchive.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2005-2008 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+
+package com.sun.tools.javac.file;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import javax.tools.JavaFileObject;
+
+import com.sun.tools.javac.file.RelativePath.RelativeDirectory;
+import com.sun.tools.javac.file.RelativePath.RelativeFile;
+import com.sun.tools.javac.util.List;
+
+public class SymbolArchive extends ZipArchive {
+
+    final File origFile;
+    final RelativeDirectory prefix;
+
+    public SymbolArchive(JavacFileManager fileManager, File orig, ZipFile zdir, RelativeDirectory prefix) throws IOException {
+        super(fileManager, zdir, false);
+        this.origFile = orig;
+        this.prefix = prefix;
+        initMap();
+    }
+
+    @Override
+    void addZipEntry(ZipEntry entry) {
+        String name = entry.getName();
+        if (!name.startsWith(prefix.path)) {
+            return;
+        }
+        name = name.substring(prefix.path.length());
+        int i = name.lastIndexOf('/');
+        RelativeDirectory dirname = new RelativeDirectory(name.substring(0, i+1));
+        String basename = name.substring(i + 1);
+        if (basename.length() == 0) {
+            return;
+        }
+        List<String> list = map.get(dirname);
+        if (list == null)
+            list = List.nil();
+        list = list.prepend(basename);
+        map.put(dirname, list);
+    }
+
+    public JavaFileObject getFileObject(RelativeDirectory subdirectory, String file) {
+        RelativeDirectory prefix_subdir = new RelativeDirectory(prefix, subdirectory.path);
+        ZipEntry ze = new RelativeFile(prefix_subdir, file).getZipEntry(zdir);
+        return new SymbolFileObject(this, file, ze);
+    }
+
+    public String toString() {
+        return "SymbolArchive[" + zdir.getName() + "]";
+    }
+
+    /**
+     * A subclass of JavaFileObject representing zip entries in a symbol file.
+     */
+    public static class SymbolFileObject extends ZipFileObject {
+        protected SymbolFileObject(SymbolArchive zarch, String name, ZipEntry entry) {
+            super(zarch, name, entry);
+        }
+
+        @Override
+        protected String inferBinaryName(Iterable<? extends File> path) {
+            String entryName = getZipEntryName();
+            String prefix = ((SymbolArchive) zarch).prefix.path;
+            if (entryName.startsWith(prefix))
+                entryName = entryName.substring(prefix.length());
+            return removeExtension(entryName).replace('/', '.');
+        }
+    }
+
+
+}
