3655efe8cfbdb8407647061035e0823e6767d018
David Cok <davidcok@users.noreply.github.com>
2009-07-15 21:32:08 +0000

    Changing the OpenJML project to use the OpenJML-jre VM environment; changed the OpenJML - 1.6 launch configuration to OpenJML-Test (since it is no longer necessarily 1.6); that launch configuration is now set to use the project's JRE, that is, OpenJML-jre.  Also some other refactoring changes.  All enabled JUnit tests are successful (for me).
    git-svn-id: file:///home/jls/openjml/repo_master/OpenJML/trunk@1529 1627bae4-3225-46cb-a19f-684862636fd9

--- OpenJML/src/com/sun/tools/javac/parser/ExpressionExtension.java
+++ OpenJML/src/com/sun/tools/javac/parser/ExpressionExtension.java
@@ -9,6 +9,11 @@ import org.jmlspecs.annotation.Nullable;
 import org.jmlspecs.openjml.JmlToken;
 import org.jmlspecs.openjml.ext.Elemtype;
 
+import com.sun.tools.javac.code.Symtab;
+import com.sun.tools.javac.code.Type;
+import com.sun.tools.javac.comp.AttrContext;
+import com.sun.tools.javac.comp.Env;
+import com.sun.tools.javac.comp.JmlAttr;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.JCTree.JCExpression;
 import com.sun.tools.javac.util.Context;
@@ -27,6 +32,9 @@ public abstract class ExpressionExtension {
     /** The scanner in use, set when derived classes are instantiated */
     protected /*@ non_null */ JmlScanner scanner;
     
+    /** The symbol table, set when the context is set */
+    protected Symtab syms;
+    
     //@ public constraint context == \old(context);
     
     /** A constructor needed by derived classes; this class should not be
@@ -39,25 +47,28 @@ public abstract class ExpressionExtension {
      * @param context the compilation context
      */
     //@ ensures this.context == context;
-    public void setContext(JmlParser parser) {
-        this.context = parser.context;
+    public void setContext(Context context, JmlParser parser) {
+        this.context = context;
         this.parser = parser;
-        this.scanner = parser.getScanner();
+        if (parser != null) this.scanner = parser.getScanner();
+        this.syms = Symtab.instance(context);
     }
     
     /** Returns a (derived instance of) ExpressionExtension if there is one associated
      * with the given token; sets its context.  Returns null if there is no
      * extension for the given token.
+     * @param pos the position of the token, for error reporting
      * @param token the extension token
      * @param context the compilation context
-     * @return an instance of a ExpressionExtension object, or null
+     * @param parser the JmlParser to be used for parsing, or null if parsing is not needed
+     * @return an instance of a ExpressionExtension object, or null if unrecoverable error
      */
-    static public @Nullable ExpressionExtension find(int pos, JmlToken token, Context context, JmlParser parser) {
+    static public @Nullable ExpressionExtension find(int pos, JmlToken token, Context context, @Nullable JmlParser parser) {
         Class<?> c = extensions.get(token);
         if (c == null) return null;
         try {
             ExpressionExtension e = (ExpressionExtension)c.newInstance();
-            e.setContext(parser);
+            e.setContext(context,parser);
             return e;
         } catch (Exception ee) {
             Log.instance(context).error(pos,"jml.failure.to.create.ExpressionExtension",token.internedName());
@@ -129,6 +140,8 @@ public abstract class ExpressionExtension {
      * @return the AST for the expression
      */
     abstract public JCExpression parse(JmlParser parser, @Nullable List<JCExpression> typeArgs);
+    
+    abstract public Type typecheck(JmlAttr attr, JCExpression expr, Env<AttrContext> env);
     
     // FIXME - I thought that here we could use Class<? extends ExpressionExtension>
     // avoiding the cast of the result of newInstance above.
