0c0f171ba79f5d2c71d52b27714ec94a79365efc
John L. Singleton <jsinglet@gmail.com>
2013-09-10 04:16:40 +0000

    Added in lattice classes for flow
    git-svn-id: file:///home/jls/openjml/repo_master/OpenJML/branches/OpenJML/the-project-formerly-known-as-safe@3020 1627bae4-3225-46cb-a19f-684862636fd9

---
+++ src/org/jmlspecs/openjml/flowspecs/JmlEETreeScanner.java
@@ -0,0 +1,120 @@
+package org.jmlspecs.openjml.flowspecs;
+
+import org.jmlspecs.openjml.JmlTree.JmlMethodClauseDecl;
+import org.jmlspecs.openjml.JmlTreeScanner;
+
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.JCTree.JCAssign;
+import com.sun.tools.javac.tree.JCTree.JCBlock;
+import com.sun.tools.javac.tree.JCTree.JCCompilationUnit;
+import com.sun.tools.javac.tree.JCTree.JCExpressionStatement;
+import com.sun.tools.javac.tree.JCTree.JCIdent;
+import com.sun.tools.javac.tree.JCTree.JCStatement;
+import com.sun.tools.javac.tree.JCTree.JCVariableDecl;
+
+/**
+ * A JML TreeScanner that uses the "enter" / "exit" pattern in the visitor. 
+ * 
+ * Notes:
+ * - That this walker only implements a certain type of traversal relevant to type checking. We should probably generalize this pattern fully and make it available to developers.
+ * - We are interested in JmlTreeScanner rather than just a tree scanner because we need to extract the method specifications 
+ * to check declassification. 
+ * 
+ * @author JLS
+ *
+ */
+public abstract class JmlEETreeScanner extends JmlTreeScanner {
+    
+    public JmlEETreeScanner() {
+        super(AST_JML_MODE);
+    }
+    
+    public void scan(JCTree tree){
+        if (tree == null) return;
+        super.scan(tree);
+    }
+    
+//    @Override
+//    public void visitJmlBinary(JmlTree.JmlBinary that) {
+//        super.visitJmlBinary(that);
+//    }
+
+//    @Override
+//    public void visitBinary(JCTree.JCBinary that) {
+//        allopcount++;
+//        System.out.println("Visiting Binary: " + that.toString());
+//        super.visitBinary(that);
+//    }
+
+    @Override
+    public void visitExec(JCExpressionStatement tree){
+        enterExec(tree);
+        super.visitExec(tree);
+        exitExec(tree);
+    }
+    
+    public abstract void enterExec(JCExpressionStatement tree);
+    public abstract void exitExec(JCExpressionStatement tree);
+    
+    @Override
+    public void visitIdent(JCIdent tree){
+        enterIdent(tree);
+        super.visitIdent(tree);
+        exitIdent(tree);
+    }
+    
+    public abstract void enterIdent(JCIdent tree);
+    public abstract void exitIdent(JCIdent tree);
+    
+    @Override
+    public void visitBlock(JCBlock tree)
+    {
+        enterBlock(tree);
+        super.visitBlock(tree);
+        exitBlock(tree);
+    }
+
+    public abstract void enterBlock(JCBlock tree);
+    public abstract void exitBlock(JCBlock tree);
+    
+    @Override
+    public void visitVarDef(JCVariableDecl tree){
+        enterVarDef(tree);
+        super.visitVarDef(tree);
+        exitVarDef(tree);
+    }
+    
+    public abstract void enterVarDef(JCVariableDecl tree);
+    public abstract void exitVarDef(JCVariableDecl tree);
+    
+    @Override
+    public void visitTopLevel(JCCompilationUnit tree){
+        enterTopLevel(tree);
+        exitTopLevel(tree);
+        
+        
+    }
+    
+    public abstract void enterTopLevel(JCCompilationUnit tree);
+    public abstract void exitTopLevel(JCCompilationUnit tree);
+
+
+    @Override
+    public void visitJmlMethodClauseDecl(JmlMethodClauseDecl tree){
+        enterJmlMethodClauseDecl(tree);
+        exitJmlMethodClauseDecl(tree);
+    }
+    
+    public abstract void enterJmlMethodClauseDecl(JmlMethodClauseDecl tree);
+    public abstract void exitJmlMethodClauseDecl(JmlMethodClauseDecl tree);
+
+    @Override
+    public void visitAssign(JCAssign tree){
+        enterAssign(tree);
+        exitAssign(tree);
+    }
+
+    public abstract void enterAssign(JCAssign tree);
+    public abstract void exitAssign(JCAssign tree);
+    
+}
