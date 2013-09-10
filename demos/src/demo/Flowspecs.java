21b7873bc94ea9632fd049c50f32558e6643ec0a
John L. Singleton <jsinglet@gmail.com>
2013-09-10 20:46:41 +0000

    Added test harness for type checking.
    git-svn-id: file:///home/jls/openjml/repo_master/OpenJML/branches/OpenJML/the-project-formerly-known-as-safe@3026 1627bae4-3225-46cb-a19f-684862636fd9

---
+++ demos/src/demo/Flowspecs.java
@@ -0,0 +1,55 @@
+package demo;
+import org.jmlspecs.annotation.*;
+public class A {
+    
+    
+    //
+    // Typechecking Tests
+    //
+    
+    
+    void test1() {
+        
+         /*@ level(PRIVATE) */ int a = 3;
+         
+         
+         /*@ level(PUBLIC) */ int b = 0;
+    
+         // ok
+         a = b;
+         // not ok
+         b = a;
+     }  
+    
+    void test2(){
+        
+        /*@ level(PRIVATE) */ int a = 3;
+        
+        
+        /*@ level(PUBLIC) */ int b = 0;
+   
+        
+        // ok
+        a = a+a;
+        b = b+b;
+        
+        // also ok
+        a = a + b;
+        a = a*2;
+        
+        // not ok
+        b = a+b;
+        b = a*2;
+        
+    }
+
+    void test3(){}
+    
+    int test3a(){}
+    
+    int test3b(){}
+    
+    
+
+}
+
