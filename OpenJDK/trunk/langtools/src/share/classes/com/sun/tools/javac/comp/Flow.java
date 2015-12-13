03afd8bd1de630fa559a2c50a2773a26571a88f8
John L. Singleton <jsinglet@gmail.com>
2015-12-12 19:38:46 -0500

    more merge fixup

--- OpenJDK/trunk/langtools/src/share/classes/com/sun/tools/javac/comp/Flow.java
+++ OpenJDK/trunk/langtools/src/share/classes/com/sun/tools/javac/comp/Flow.java
@@ -1495,6 +1495,7 @@ public class Flow {
          *  I.e. is symbol either a local or a blank final variable?
          */
         protected boolean trackable(VarSymbol sym) {
+            if (sym == null) return false; // DRC - added - explore why sym is null for JMLDataGroup FIXME
             return
                 sym.pos >= startPos &&
                 ((sym.owner.kind == MTH ||
@@ -2568,9 +2569,10 @@ public class Flow {
         @Override
         public void visitIdent(JCIdent tree) {
             if (tree.sym != null && tree.sym.kind == VAR) { // DRC - Guarding this check, but why is the variable unattributed?
-                checkInit(tree.pos(), (VarSymbol)tree.sym);
+                checkEffectivelyFinal(tree, (VarSymbol)tree.sym);
             }
         }
+        
 
         public void visitAssign(JCAssign tree) {
             JCTree lhs = TreeInfo.skipParens(tree.lhs);
