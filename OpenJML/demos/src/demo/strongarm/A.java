bbbe5200c9f6dd3bc2e9575c46169283928b3c73
John L. Singleton <jsinglet@gmail.com>
2016-05-11 12:42:35 -0400

    added examples directory and first example

---
+++ OpenJML/demos/src/demo/strongarm/A.java
@@ -0,0 +1,30 @@
+package demo.strongarm;
+
+/**
+ * 
+ * Category: Non-interprocedural, loop-free
+ * Features: locals, primatives
+ * 
+ * @author jls
+ *
+ */
+public class A {
+    
+    //@ requires true;
+    public int cmp(int a, int b){
+        if(a < b){
+            return -1;
+        }else{
+            
+            if(a > b){
+                return 1;
+            }
+            
+            return 0;
+            
+        }
+    }
+    
+    
+}
+
