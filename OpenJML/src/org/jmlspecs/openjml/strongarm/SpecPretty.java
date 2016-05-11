a217fe0be433c54b624f7e98239718601b629ab5
John L. Singleton <jsinglet@gmail.com>
2016-05-11 13:23:57 -0400

    Fixed spec writing to preserve imports and package information

--- OpenJML/src/org/jmlspecs/openjml/strongarm/SpecPretty.java
+++ OpenJML/src/org/jmlspecs/openjml/strongarm/SpecPretty.java
@@ -1,20 +1,23 @@
 package org.jmlspecs.openjml.strongarm;
 
+import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 
 import org.jmlspecs.annotation.NonNull;
 import org.jmlspecs.openjml.JmlPretty;
+import org.jmlspecs.openjml.JmlTree.JmlClassDecl;
 import org.jmlspecs.openjml.JmlTree.JmlMethodDecl;
 
 import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.JCTree.JCCompilationUnit;
 
 
 public class SpecPretty extends JmlPretty {
 
     public SpecPretty(Writer out, boolean sourceOutput) {
         super(out, sourceOutput);
-       
+     
     }
     
     /** we need to remove the bodies **/
@@ -24,11 +27,18 @@ public class SpecPretty extends JmlPretty {
         super.visitJmlMethodDecl(that);
     }
     
-    static public @NonNull String write(@NonNull JCTree tree, boolean source) {
+    static public @NonNull String write(@NonNull JmlClassDecl tree, boolean source) {
         StringWriter sw = new StringWriter();
         JmlPretty p = new SpecPretty(sw,source);
         p.width = 2;
-        tree.accept(p);
+
+        // we don't want this
+        /* tree.accept(p); */
+        
+        // we want to preserve the imports and package statements that already exist
+        // (this requires we start at the toplevel)
+        p.visitTopLevel(tree.toplevel);
+        
         return sw.toString();
     }
 
