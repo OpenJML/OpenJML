62ee612f1c22c114dedb24b15a34e3289c2315f0
John L. Singleton <jsinglet@gmail.com>
2016-05-19 15:33:41 -0400

    added new example for edge case

--- OpenJML/demos/src/demo/strongarm/A3.java
+++ OpenJML/demos/src/demo/strongarm/A3.java
@@ -11,6 +11,7 @@ package demo.strongarm;
 public class A3 {
     
     int THE_FIELD;
+    int ANOTHER_FIELD;
     
     //@ requires true;
     public int localTest(int a, int b){
@@ -25,6 +26,22 @@ public class A3 {
         return -1;
     }
     
-    
+
+     
+    //@ requires true;
+    public int localTest2(int a, int b){
+        
+        THE_FIELD = 999;
+
+        if(a > -1){
+            return 0;
+        }
+        
+        ANOTHER_FIELD = THE_FIELD*10;
+        
+        THE_FIELD = 777;
+        return -1;
+    }
+
 }
 
