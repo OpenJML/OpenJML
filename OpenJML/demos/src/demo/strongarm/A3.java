1ae2d06036eabff01473e77f8ab41437e40969c4
John L. Singleton <jsinglet@gmail.com>
2016-05-24 20:07:14 -0400

    adding examples to handle multiple assignment and other tricky cases.

--- OpenJML/demos/src/demo/strongarm/A3.java
+++ OpenJML/demos/src/demo/strongarm/A3.java
@@ -11,37 +11,68 @@ package demo.strongarm;
 public class A3 {
     
     int THE_FIELD;
-    int ANOTHER_FIELD;
     
-    //@ requires true;
-    public int localTest(int a, int b){
-        
-        THE_FIELD = 999;
+    class A3c {
+        public int ANOTHER_FIELD;
+    }
 
-        if(a > -1){
-            return 0;
-        }
+    //@ requires true;
+    public int localTest0(){
         
-        THE_FIELD = 777;
+        int a;
+        
+        a = 3;
+        a = 4;
+        a = 5;
+        
         return -1;
     }
-    
 
-     
     //@ requires true;
-    public int localTest2(int a, int b){
+    public int localTest1(){
         
-        THE_FIELD = 999;
-
+        int a=0;
+        
         if(a > -1){
-            return 0;
+            a = 3;
+            a = 4;
         }
         
-        ANOTHER_FIELD = THE_FIELD*10;
+        a = 5;
         
-        THE_FIELD = 777;
         return -1;
     }
+
+    
+    // @ requires true;
+//    public int localTest(int a, int b){
+//        
+//        THE_FIELD = 999;
+//
+//        if(a > -1){
+//            return 0;
+//        }
+//        
+//        THE_FIELD = 777;
+//        return -1;
+//    }
+//    
+//
+//     
+//    //@ requires true;
+//    public int localTest2(int a, int b, A3c otherClazz){
+//        
+//        THE_FIELD = 999;
+//
+//        if(a > -1){
+//            return 0;
+//        }
+//        
+//        otherClazz.ANOTHER_FIELD = THE_FIELD*10;
+//        
+//        THE_FIELD = 777;
+//        return -1;
+//    }
 
 }
 
