3e07ff0f09731e00959c36ca7794bf37770365ee
davidcok <cok@frontiernet.net>
2016-09-23 11:59:27 -0400

    Version and build for 0.7.2

--- OpenJML/release-branches.sh
+++ OpenJML/release-branches.sh
@@ -15,16 +15,50 @@ fi
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 ( ( ls openjml.properties > /dev/null ) || ( echo "Not in correct directory"; pwd; exit 1 ) )
 
-git checkout -b $REL
+git checkout -b "$REL"
 cd ../../JMLAnnotations
-git checkout -b $REL
+git checkout -b "$REL"
 cd ../OpenJMLDemo
-git checkout -b $REL
+git checkout -b "$REL"
 cd ../Specs
-git checkout -b $REL
-cd ../OpenJML-UpdateSite -b $REL
-git checkout -b $REL
+git checkout -b "$REL"
+cd ../OpenJML-UpdateSite -b "$REL"
+git checkout -b "$REL"
 cd ../OpenJML/OpenJML
 
 ant -f build-bash.xml release
+
+## Starting in .../OpenJML/OpenJML
+## Should be in the release branch
+
+git add .
+git commit -a -m "$REL"
+git push --set-upstream origin "$REL"
+git checkout master
+
+cd ../../Specs
+git add .
+git commit -a -m "$REL"
+git push --set-upstream origin "$REL"
+git checkout master
+
+cd ../JMLAnnotations
+git add .
+git commit -a -m "$REL"
+git push --set-upstream origin "$REL"
+git checkout master
+
+cd ../JmlOpenJMLDemo
+git add .
+git commit -a -m "$REL"
+git push --set-upstream origin "$REL"
+git checkout master
+
+cd ../OpenJML-UpdateSite
+git add .
+git commit -a -m "$REL"
+git push --set-upstream origin "$REL"
+git checkout master
+
+cd ../OpenJML/OpenJML
 
