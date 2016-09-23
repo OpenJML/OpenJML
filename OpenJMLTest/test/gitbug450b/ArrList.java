7881425f0302945c47df65cec6665ba0306178ea
iconmaster5326 <crazybonesj@yahoo.com>
2016-09-23 09:07:20 -0400

    Fixed test case 450

--- OpenJMLTest/test/gitbug450b/ArrList.java
+++ OpenJMLTest/test/gitbug450b/ArrList.java
@@ -3,14 +3,14 @@
 import java.util.ArrayList;
 
 public class ArrList {
-    public ArrayList<String> theList;
+    public ArrayList<Integer> theList;
 
     // Use default assignable, else use assignable theList, theList.*;
     //@ ensures theList != null;
     //@ ensures theList.size() == 1;
     // @ ensures theList.indexOf(a) == 0; // FIXME - at this time, the spec of ArrayList (and List) is not adequate to prove this assertion
     public ArrList(int a) {
-        theList = new ArrayList<String>();
+        theList = new ArrayList<Integer>();
         theList.add(a);
     }
 
