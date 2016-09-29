7617e48a7566d929075d3de9e7b7558038fd72e1
John L. Singleton <jsinglet@gmail.com>
2016-09-28 21:22:26 -0400

    so many unit tests

---
+++ OpenJMLTest/src/org/jmlspecs/openjmltest/testcases/strongarm.java
@@ -0,0 +1,153 @@
+package org.jmlspecs.openjmltest.testcases;
+
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jmlspecs.openjmltest.EscBase;
+import org.jmlspecs.openjmltest.StrongarmBase;
+import org.jmlspecs.openjmltest.TCBase;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.ParameterizedWithNames;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(ParameterizedWithNames.class)
+public class strongarm extends StrongarmBase {
+
+    // @Override
+    // public void setUp() throws Exception {
+    //// noCollectDiagnostics = true;
+    //// jmldebug = true;
+    // super.setUp();
+    // }
+
+    public strongarm(String options, String solver) {
+	super(options, solver);
+    }
+
+    @Parameters
+    static public Collection<String[]> parameters() {
+	return minQuantAndSolvers(solvers);
+    }
+
+    public void helpSA(String sourceDirname, String outDir, String... opts) {
+	onFile(sourceDirname, outDir, opts);
+    }
+
+    @Test
+    public void testA() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A.java", "test/strongarm/base/");
+    }
+
+    @Test
+    public void testA1() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A1.java", "test/strongarm/base/");
+    }
+
+    @Test
+    public void testA2() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A2.java", "test/strongarm/base/");
+    }
+    @Test
+    public void testA3() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A3.java", "test/strongarm/base/");
+    }
+    @Test
+    public void testA4() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A4.java", "test/strongarm/base/");
+    }
+    @Test
+    public void testA5() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A5.java", "test/strongarm/base/");
+    }
+    @Test
+    public void testA6() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A6.java", "test/strongarm/base/");
+    }
+    @Test
+    public void testA7() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A7.java", "test/strongarm/base/");
+    }
+    @Test
+    public void testA8() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A8.java", "test/strongarm/base/");
+    }
+    @Test
+    public void testA9() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A9.java", "test/strongarm/base/");
+    }
+    @Test
+    public void testA10() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A10.java", "test/strongarm/base/");
+    }
+    @Test
+    public void testA11() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A11.java", "test/strongarm/base/");
+    }
+    @Test
+    public void testA12() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A12.java", "test/strongarm/base/");
+    }
+    @Test
+    public void testA13() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A13.java", "test/strongarm/base/");
+    }
+    @Test
+    public void testA14() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A14.java", "test/strongarm/base/");
+    }
+    @Test
+    public void testA15() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/A15.java", "test/strongarm/base/");
+    }
+    @Test
+    public void testB() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/B.java", "test/strongarm/base/");
+    }
+
+    
+    @Test
+    public void testC1() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/C1.java", "test/strongarm/base/");
+    }
+
+    @Test
+    public void testD() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/D.java", "test/strongarm/base/");
+    }
+    
+    @Test
+    public void testE1() {
+	//expectedExit = 1;
+	helpSA("test/strongarm/base/E1.java", "test/strongarm/base/");
+    }
+
+
+    
+}
