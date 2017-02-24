36a654ee6f23ba281c25d6ede9d09117962301b2
John L. Singleton <jsinglet@gmail.com>
2017-02-24 00:25:13 -0500

    more work on factor analysis

--- OpenJML/src/org/jmlspecs/openjml/strongarm/tree/analysis/FactorExpressionsAnalysis.java
+++ OpenJML/src/org/jmlspecs/openjml/strongarm/tree/analysis/FactorExpressionsAnalysis.java
@@ -13,10 +13,14 @@ import org.jmlspecs.openjml.JmlTree.JmlMethodClause;
 import org.jmlspecs.openjml.JmlTree.JmlMethodClauseDecl;
 import org.jmlspecs.openjml.JmlTree.JmlMethodClauseExpr;
 import org.jmlspecs.openjml.JmlTree.JmlMethodClauseGroup;
+import org.jmlspecs.openjml.JmlTree.JmlMethodClauseStoreRef;
 import org.jmlspecs.openjml.JmlTree.JmlSpecificationCase;
 import org.jmlspecs.openjml.strongarm.Strongarm;
 
 import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.JCTree.JCBinary;
+import com.sun.tools.javac.tree.JCTree.JCExpression;
+import com.sun.tools.javac.tree.JCTree.JCIdent;
 import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.List;
 import com.sun.tools.javac.util.Log.WriterKind;
@@ -27,177 +31,332 @@ public class FactorExpressionsAnalysis extends JmlTreeAnalysis {
         super(context);
     }
 
-    private ArrayList<JmlMethodClauseGroup> path = new ArrayList<JmlMethodClauseGroup>();
-    private Map<JmlMethodClauseGroup, Set<JmlMethodClauseExpr>> joins = new HashMap<JmlMethodClauseGroup, Set<JmlMethodClauseExpr>>();
-    
-    private ArrayList<JmlMethodClauseGroup> reverseGraph(){
-        
-        ArrayList<JmlMethodClauseGroup> r = new ArrayList<JmlMethodClauseGroup>(path);
-        
+    private ArrayList<JmlMethodClauseGroup>                     path  = new ArrayList<JmlMethodClauseGroup>();
+
+    private Map<JmlMethodClauseGroup, Set<JmlMethodClause>> joins = new HashMap<JmlMethodClauseGroup, Set<JmlMethodClause>>();
+
+    private ArrayList<JmlMethodClauseGroup> reverseGraph() {
+
+        ArrayList<JmlMethodClauseGroup> r = new ArrayList<JmlMethodClauseGroup>(
+                path);
+
         Collections.reverse(r);
-        
+
         return r;
     }
 
-    private void printIntersectionMap(){
-        
-        if(verbose){
-        
-            for(JmlMethodClauseGroup k : joins.keySet()){
-                
+    private void printIntersectionMap() {
+
+        if (verbose) {
+
+            for (JmlMethodClauseGroup k : joins.keySet()) {
+
                 log.getWriter(WriterKind.NOTICE).println(">Group Start<");
 
                 log.getWriter(WriterKind.NOTICE).println(k.toString());
 
                 log.getWriter(WriterKind.NOTICE).println("Intersections:");
-                
-                for(JmlMethodClauseExpr e : joins.get(k)){
-                    log.getWriter(WriterKind.NOTICE).println("\t -> " + e.toString());
+
+                for (JmlMethodClause e : joins.get(k)) {
+                    log.getWriter(WriterKind.NOTICE)
+                            .println("\t -> " + e.toString());
                 }
 
                 log.getWriter(WriterKind.NOTICE).println(">Group End<");
 
             }
-            
+
         }
     }
 
-    private void doAnalysis(JCTree tree){
+    private void doAnalysis(JCTree tree) {
         // step 1
         // find the reverse graph
         scan(tree);
-        
+
         ArrayList<JmlMethodClauseGroup> R = reverseGraph();
-        
+
         // step 2
-        // next, for each join (OR), find the intersection of the sets of propositions. 
-        // for each join point, we require that (if it's not a block of statements) that 
-        // the join point has been previously computed. 
-                
+        // next, for each join (OR), find the intersection of the sets of
+        // propositions.
+        // for each join point, we require that (if it's not a block of
+        // statements) that
+        // the join point has been previously computed.
+
         Map<JmlMethodClauseGroup, Set<JmlMethodClauseExpr>> blockMap = new HashMap<JmlMethodClauseGroup, Set<JmlMethodClauseExpr>>();
-        
-        while(R.size() > 0){ // fixpoint 
-            for (Iterator<JmlMethodClauseGroup> iterator = R.iterator(); iterator.hasNext();) {
-                
+
+        while (R.size() > 0) { // fixpoint
+            for (Iterator<JmlMethodClauseGroup> iterator = R
+                    .iterator(); iterator.hasNext();) {
+
                 JmlMethodClauseGroup G = iterator.next();
-                
-                Set<JmlMethodClauseExpr> intersection = getIntersection(G);
-                
-                if(intersection!=null){
+
+                Set<JmlMethodClause> intersection = getIntersection(G);
+
+                if (intersection != null) {
                     joins.put(G, intersection);
                     iterator.remove();
                 }
-                
-                
+
             }
         }
 
         printIntersectionMap();
         
+        log.getWriter(WriterKind.NOTICE).println(">==========================<");
+
+
         // step 3 "bubbling"
-        // at this point, we should have computed all of the intersections. 
-        // we could stop here and have a slightly reduced contract; however, to maximize 
-        // the reduction in the size of the contract, we "bubble" up things in the intersection 
-        // that exist in the intersection of a child clause, but not necessarily in the parent. 
-        // of course, it is possible that contradictory statements could be in the group we are trying 
-        // to bubble to that contradict the clause we are bubbling. to prevent this, we make sure there 
+        // at this point, we should have computed all of the intersections.
+        // we could stop here and have a slightly reduced contract; however, to
+        // maximize
+        // the reduction in the size of the contract, we "bubble" up things in
+        // the intersection
+        // that exist in the intersection of a child clause, but not necessarily
+        // in the parent.
+        // of course, it is possible that contradictory statements could be in
+        // the group we are trying
+        // to bubble to that contradict the clause we are bubbling. to prevent
+        // this, we make sure there
         // are no conflicts prior to adding it to the intersections.
-        
-        
-        
+
+        // the effect of this stage is to increase the size of the intersection
+        // set
+        // we repeat the bubbling until the size fo the intersections doesn't
+        // change.
+        int previousSize = 0;
+        do {
+            previousSize = joins.values().stream().mapToInt(x -> x.size()).sum();
+
+            R = reverseGraph();
+
+            for (Iterator<JmlMethodClauseGroup> iterator = R
+                    .iterator(); iterator.hasNext();) {
+
+                JmlMethodClauseGroup G = iterator.next();
+
+                bubble(G);
+
+            }
+
+        } while (joins.values().stream().mapToInt(x -> x.size()).sum() != previousSize);
+
+        printIntersectionMap();
+
     }
 
-    private Set<JmlMethodClauseExpr> getIntersection(JmlMethodClauseGroup tree) {
+    private void bubble(JmlMethodClauseGroup tree) {
 
-        // the precondition for getting the intersection is that either 
-        // a) ALL parts of the group are lists of expressions
-        // or 
-        // b) We have computed an intersection for the members of the list
-        // that ARE NOT just lists of expressions. 
-        //.cases
+        Set<JmlMethodClause> intersection = joins.get(tree);
+        JmlSpecificationCase last = null;
         
-        Set<JmlMethodClauseExpr> intersection = new HashSet<JmlMethodClauseExpr>();
-        
-        for(List<JmlSpecificationCase> cases = tree.cases; cases.nonEmpty(); cases = cases.tail ){
-            
-            
+        for (List<JmlSpecificationCase> cases = tree.cases; cases
+                .nonEmpty(); cases = cases.tail) {
+
             Set<JmlMethodClauseExpr> thisGroup = new HashSet<JmlMethodClauseExpr>();
+
+            
+            // it's nested (but must be previously computed)
+            // this is the case we are looking at expanding.
+            if (cases.head.clauses.head instanceof JmlMethodClauseGroup) {
+                
+                Set<JmlMethodClause> join = joins
+                        .get(cases.head.clauses.head);
+
+                // if for some reason there isn't a previous 
+                // set, let's create it now.
+                if(join == null){
+                    join = new HashSet<JmlMethodClause>();
+                    joins.put((JmlMethodClauseGroup)cases.head.clauses.head, join);
+
+                }
+
+                
+                // what we are looking for here 
+                // are things that are in the intersection of some child group
+                // if the PARENT doesn't explicity say something 
+                // about the variables in the given expression 
+                // we add it to the intersections.
+                
+                computeAdditiveIntersection(last, intersection, join);
+            }else{
+                last = cases.head;
+            }
+
+        }
+        
+        joins.put(tree,  intersection);
+
+    }
+
+    private void computeAdditiveIntersection(
+            JmlSpecificationCase last, Set<JmlMethodClause> intersection,
+            Set<JmlMethodClause> B) {
+       
+        
+        Map<String, JmlMethodClause> us = new HashMap<String, JmlMethodClause>();
+        Map<String, JmlMethodClause> them = new HashMap<String, JmlMethodClause>();
+
+        for (JmlMethodClause e : intersection) {
+            us.put(e.toString(), e);
+        }
+
+        for (JmlMethodClause e : B) {
+            them.put(e.toString(), e);
+            
+            // convert subexpressions
+            Set<JCIdent> idents = new HashSet<JCIdent>();
+            
+            if(e instanceof JmlMethodClauseExpr){
+                idents = GetIdentsAnalysis.analyze(((JmlMethodClauseExpr)e).expression);
+            }else if(e instanceof JmlMethodClauseStoreRef) {
+                
+                JmlMethodClauseStoreRef ref = (JmlMethodClauseStoreRef)e;
+                
+                idents = GetIdentsAnalysis.analyze(ref.list);
+            }
+            
+            for(JCIdent id : idents){
+                them.put(id.toString(), e);
+            }
+            
+            
+        }
+        
+        Set<JCIdent> parentIdents = GetIdentsAnalysis.analyze(last);
+        Set<String> parents = new HashSet<String>();
+
+        for(JCIdent p : parentIdents){
+            parents.add(p.toString());
+        }
+        
+        // look for things that are in B, but not in any subexpression in intersection. 
+        for(String t : them.keySet()){
+            
+            if(keyContains(t, us.keySet())==false && intersection.contains(them.get(t))==false){
+                
+                // for the moment, we do something not so brilliant
+                // in that we consider ANY reference of an ident 
+                // to violate data integrity. 
+               
+                if(parents.contains(t)==false && (keyContains(t, parents)==false || them.get(t) instanceof JmlMethodClauseStoreRef) ){
+                    intersection.add(them.get(t));
+                }
+                
+                
+            }
             
                 
+        }
+        
+    }
+    
+    
+    
+    private boolean keyContains(String needle, Set<String> haystack)
+    {
+        for(String s : haystack){
+            if(s.contains(needle) || needle.contains(s)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Set<JmlMethodClause> getIntersection(
+            JmlMethodClauseGroup tree) {
+
+        // the precondition for getting the intersection is that either
+        // a) ALL parts of the group are lists of expressions
+        // or
+        // b) We have computed an intersection for the members of the list
+        // that ARE NOT just lists of expressions.
+        // .cases
+
+        Set<JmlMethodClause> intersection = new HashSet<JmlMethodClause>();
+
+        for (List<JmlSpecificationCase> cases = tree.cases; cases
+                .nonEmpty(); cases = cases.tail) {
+
+            Set<JmlMethodClause> thisGroup = new HashSet<JmlMethodClause>();
+
             // it's nested (but must be previously computed)
-            if(cases.head.clauses.head instanceof JmlMethodClauseGroup){
-                Set<JmlMethodClauseExpr> join = joins.get(cases.head.clauses.head);
-                
-                if(join==null){
+            if (cases.head.clauses.head instanceof JmlMethodClauseGroup) {
+                Set<JmlMethodClause> join = joins
+                        .get(cases.head.clauses.head);
+
+                if (join == null) {
                     return null;
-                }else{
+                } else {
                     thisGroup.addAll(join);
                 }
             }
-            // case 2, it's a set of all JmlSpecificationCases comprised of MethodClauseExpressions
-            else{
-                
-                
-                for(List<JmlMethodClause> clauses = cases.head.clauses; clauses.nonEmpty(); clauses = clauses.tail){
-                    
-                    if(clauses.head instanceof JmlMethodClauseExpr){
-                        thisGroup.add((JmlMethodClauseExpr)clauses.head);
-                    }                    
+            // case 2, it's a set of all JmlSpecificationCases comprised of
+            // MethodClauseExpressions
+            else {
+
+                for (List<JmlMethodClause> clauses = cases.head.clauses; clauses
+                        .nonEmpty(); clauses = clauses.tail) {
+
+                    if (clauses.head instanceof JmlMethodClauseExpr || clauses.head instanceof JmlMethodClauseStoreRef) {
+                        thisGroup.add(clauses.head);
+                    }
                 }
-                
+
             }
-            if(intersection.size()==0){
+            if (intersection.size() == 0) {
                 intersection.addAll(thisGroup);
-            }else{
+            } else {
                 computeIntersectionWith(intersection, thisGroup);
             }
-            
+
         }
-        
+
         return intersection;
     }
 
-    public void computeIntersectionWith(Set<JmlMethodClauseExpr> intersection, Set<JmlMethodClauseExpr> thisGroup){
-        
-        Map<String,JmlMethodClauseExpr> us = new HashMap<String,JmlMethodClauseExpr>();
-        Map<String,JmlMethodClauseExpr> them = new HashMap<String,JmlMethodClauseExpr>();
-        
-        for(JmlMethodClauseExpr e : intersection){
+    public void computeIntersectionWith(Set<JmlMethodClause> intersection,
+            Set<JmlMethodClause> thisGroup) {
+
+        Map<String, JmlMethodClause> us = new HashMap<String, JmlMethodClause>();
+        Map<String, JmlMethodClause> them = new HashMap<String, JmlMethodClause>();
+
+        for (JmlMethodClause e : intersection) {
             us.put(e.toString(), e);
         }
-        
-        for(JmlMethodClauseExpr e : thisGroup){
+
+        for (JmlMethodClause e : thisGroup) {
             them.put(e.toString(), e);
         }
-        
-        for(String s : us.keySet()){
-            if(them.keySet().contains(s)==false){
+
+        for (String s : us.keySet()) {
+            if (them.keySet().contains(s) == false) {
                 intersection.remove(us.get(s));
             }
         }
-        
+
     }
 
-    // step 1 here is to find the reverse graph. this is used just to find that. 
+    // step 1 here is to find the reverse graph. this is used just to find that.
     @Override
     public void visitJmlMethodClauseGroup(JmlMethodClauseGroup tree) {
-        //log.getWriter(WriterKind.NOTICE).println(">Group Start<");
-        //log.getWriter(WriterKind.NOTICE).println(tree.toString());
+        // log.getWriter(WriterKind.NOTICE).println(">Group Start<");
+        // log.getWriter(WriterKind.NOTICE).println(tree.toString());
         path.add(tree);
-        //log.getWriter(WriterKind.NOTICE).println(">Group End<");
+        // log.getWriter(WriterKind.NOTICE).println(">Group End<");
 
         super.visitJmlMethodClauseGroup(tree);
     }
-    
-    public static void analyze(JCTree tree){
-        
-        FactorExpressionsAnalysis analysis = new FactorExpressionsAnalysis(Strongarm._context);
+
+    public static void analyze(JCTree tree) {
+
+        FactorExpressionsAnalysis analysis = new FactorExpressionsAnalysis(
+                Strongarm._context);
         analysis.doAnalysis(tree);
     }
 
+    public static void analyze(JCTree tree, Context ctx, JmlTokenKind kind) {
 
-    public static void analyze(JCTree tree, Context ctx, JmlTokenKind kind){
-        
         FactorExpressionsAnalysis analysis = new FactorExpressionsAnalysis(ctx);
         analysis.doAnalysis(tree);
     }
