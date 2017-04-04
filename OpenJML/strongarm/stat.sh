f4d0be793771a6c0fe75fb631d31bf8f9e08f330
John L. Singleton <jsinglet@gmail.com>
2017-04-04 17:12:20 -0400

    added this super helpful status script

---
+++ OpenJML/strongarm/stat.sh
@@ -0,0 +1,28 @@
+#!/usr/local/bin/bash
+
+
+EVALS=( "junit4" "json-java" "commons-csv" "commons-cli"  )
+
+BUFFER="Test Started"
+
+for E in "${EVALS[@]}"
+do
+
+    # compute started, completed, and aborted.
+    RAW=run.out.$E
+
+
+
+    if [ -f $RAW ]; then
+
+        file=$RAW
+
+        started=$(grep "STARTING INFERENCE" $file | wc -l)
+
+        BUFFER=$BUFFER$'\n'"$E $started"
+
+    fi
+done
+
+
+echo "$BUFFER" | column -t
