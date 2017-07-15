5a336adbd0d93c3a10a30a71ed6a9357b53bf8fc
davidcok <cok@frontiernet.net>
2017-07-14 21:06:05 -0400

    Conflict resolved

--- OpenJML/src/org/jmlspecs/openjml/esc/JmlAssertionAdder.java
+++ OpenJML/src/org/jmlspecs/openjml/esc/JmlAssertionAdder.java
@@ -3246,7 +3246,6 @@ public class JmlAssertionAdder extends JmlTreeScanner {
                 if (!utils.jmlvisible(vd.sym, methodDecl.sym.owner, csym, vd.mods.flags, methodDecl.mods.flags)) continue;
 
                 if (utils.isJMLStatic(vd.sym) && isFinal(vd.sym)) {
-                    if (vd.sym.toString().contains("CHILD") || vd.sym.toString().contains("FIELD")) Utils.stop();
                     alreadyDiscoveredFields.add(vd.sym);
                     Symbol sym = vd.sym;
                     if (sym != null && sym.owner instanceof ClassSymbol) {
@@ -6623,7 +6622,7 @@ public class JmlAssertionAdder extends JmlTreeScanner {
                 }
                 
                 // Ensure determinism
-                if (esc && newclass == null && resultType != null && oldenv == null) {
+                if (esc && newclass == null && resultType != null && oldenv == null) xx: {
                     // Make an encoded name
                     // FIXME - we should include resolved type variables in the mangled name; also need to distinguish methods with the same name
                     String loc = mspecs != null && mspecs.decl != null ? Integer.toString(mspecs.decl.pos) : "00" ; // FIXME -if no position we run the risk of name conflict; should also qualify by the file 
@@ -6652,6 +6651,7 @@ public class JmlAssertionAdder extends JmlTreeScanner {
                     }
                     int argNumber = 1;
                     for (JCExpression arg: trArgs) {
+                        if (arg instanceof JCLambda) break xx; // if there is a Lambda expression just abandon this saving of values
                     	String nm0 = nm + "__ARG" + (argNumber++);
                     	if (nm0.startsWith("__JMLSaved_java_lang_CharSequence_equal__H3__ARG2")) Utils.stop();
                     	VarSymbol v = getDeterminismSymbol(calleeMethodSym,nm0);
@@ -9978,7 +9978,6 @@ public class JmlAssertionAdder extends JmlTreeScanner {
             if (utils.isJMLStatic(sym)) newfa = treeutils.makeSelect(that.pos, treeutils.makeType(that.pos, sym.owner.type), sym);
             else newfa = treeutils.makeSelect(that.pos, currentThisExpr, sym);
         }
-        if (sym.toString().contains("CHILD") || sym.toString().contains("FIELD")) Utils.stop();
         if (!rac && sym != null && alreadyDiscoveredFields.add(sym)) { // true if s was NOT in the set already
             if (utils.isJMLStatic(sym) && isFinal(sym)) {
                 addFinalStaticField(newfa);
