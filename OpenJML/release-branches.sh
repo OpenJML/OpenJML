0430a6a46baf5b1a7c56350c8723086b749d036f
davidcok <davidcok@github.com>
2017-08-29 09:25:35 -0400

    0.8.20

--- OpenJML/release-branches.sh
+++ OpenJML/release-branches.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-cd "$( dirname "${BASH_SOURCE[0]}" )"
+d=$(dirname "$BASH_SOURCE[0]")
+cd "$d"
 ( ( ls openjml.properties > /dev/null ) || ( echo "Not in correct directory"; pwd; exit 1 ) )
 
 REFBRANCH=`git rev-parse --abbrev-ref HEAD`
