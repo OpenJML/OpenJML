f744f2da6d6157a71a9ec67f3b1a85b8e2bc396b
davidcok <davidcok@github.com>
2017-10-15 18:35:46 -0400

    Adding test #580

--- OpenJMLTest/src/org/jmlspecs/openjmltest/testcases/SFBugs.java
+++ OpenJMLTest/src/org/jmlspecs/openjmltest/testcases/SFBugs.java
@@ -581,6 +581,11 @@ public class SFBugs extends EscBase {
         helpTCG();
     }
     
+    @Test public void gitbug580() {
+        expectedExit = 0;
+        helpTCG();
+    }
+    
     public void gitbug888() {
     	expectedExit = 0;
         helpTCG();
