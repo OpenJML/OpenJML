f4defe2a44c71ceba58e5c38a5c329d26b17b2db
davidcok <davidcok@github.com>
2019-05-05 20:28:35 +0200

    Adding a location type

--- OpenJMLTest/test/valuetypes/Locset.java
+++ OpenJMLTest/test/valuetypes/Locset.java
@@ -7,33 +7,33 @@ public class Locset {
     //@ ensures locset.locset().empty();
     //@ model public static void newLocSetIsEmpty() {}
     
-//    //@ public normal_behavior
-//    //@ ensures locset.locset().add(o).size() == 1;
-//    //@ model public static void singleton(T o) {}
-//    
-//    //@ public normal_behavior
-//    //@ ensures !s.contains(o) ==> s.add(o).size() == 1 + s.size();
-//    //@ model public static <T> void addBumpsSize(set<T> s, T o) {}
-//    
-//    //@ public normal_behavior
-//    //@ ensures s.contains(o) ==> s.add(o).size() == s.size();
-//    //@ model public static <T> void addDoesNotChangeSize(set<T> s, T o) {}
-//    
-//    //@ public normal_behavior
-//    //@ ensures !s.contains(o) ==> set.equals(s.add(o).remove(o), s);
-//    //@ model public static <T> void addRemove(set<T> s, T o) {}
-//    
-//    //@ public normal_behavior
-//    //@ ensures s.contains(o) ==> set.equals(s.add(o), s);
-//    //@ model public static <T> void addNoChange(set<T> s, T o) {}
-//    
-//    //@ public normal_behavior
-//    //@ ensures !s.contains(o) ==> set.equals(s, s.remove(o));
-//    //@ model public static <T> void addRemoveA(set<T> s, T o) {}
-//    
-//    //@ public normal_behavior
-//    //@ ensures s.contains(o) ==> s.remove(o).size() == s.size() - 1;
-//    //@ model public static <T> void addRemoveB(set<T> s, T o) {}
+    //@ public normal_behavior
+    //@ ensures locset.locset().add(o).size() == 1;
+    //@ model public static void singleton(location o) {}
+    
+    //@ public normal_behavior
+    //@ ensures !s.contains(o) ==> s.add(o).size() == 1 + s.size();
+    //@ model public static void addBumpsSize(locset s, location o) {}
+    
+    //@ public normal_behavior
+    //@ ensures s.contains(o) ==> s.add(o).size() == s.size();
+    //@ model public static void addDoesNotChangeSize(locset s, location o) {}
+    
+    //@ public normal_behavior
+    //@ ensures !s.contains(o) ==> locset.equals(s.add(o).remove(o), s);
+    //@ model public static void addRemove(locset s, location o) {}
+    
+    //@ public normal_behavior
+    //@ ensures s.contains(o) ==> locset.equals(s.add(o), s);
+    //@ model public static void addNoChange(locset s, location o) {}
+    
+    //@ public normal_behavior
+    //@ ensures !s.contains(o) ==> locset.equals(s, s.remove(o));
+    //@ model public static void addRemoveA(locset s, location o) {}
+    
+    //@ public normal_behavior
+    //@ ensures s.contains(o) ==> s.remove(o).size() == s.size() - 1;
+    //@ model public static void addRemoveB(locset s, location o) {}
     
     void m() {
         //@ ghost locset s = locset.locset();
