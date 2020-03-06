4bd1f8015c2d1dc0d7f7e7236a377fe4c14f0443
davidcok <davidcok@github.com>
2020-03-06 09:08:58 -0500

    Fixes for use of 'code' keyword

--- OpenJMLTest/test/factorial/Factorial.java
+++ OpenJMLTest/test/factorial/Factorial.java
@@ -2,7 +2,7 @@
      
     public class Factorial
     {
-       //@ requires n >= 0 && n <= 20;
+       //@ requires n >= 0 && n <= 19;
        //@ ensures \result == spec_factorial(n);
        public long Facto(int n)
        {
@@ -19,11 +19,15 @@
       //@ maintaining fact > 0;
       //@ maintaining fact <= Long.MAX_VALUE; 
       //@ maintaining spec_factorial(c - 1) == fact;
+       //@ loop_writes c, fact;
       //@ decreases n - c;
       for (c = 1; c <= n; c++){
+         //@ assert spec_factorial(c-1) == fact;
          //@ assert c <= n;
          //@ assume fact*c <= Long.MAX_VALUE;
          fact = fact*c;
+         //@ assert spec_factorial(c) == c * spec_factorial(c-1);
+         //@ assert spec_factorial(c) == fact;
       }
       //@ assert c == n+1;
       //@ assert spec_factorial(c - 1) == fact;   
@@ -36,7 +40,7 @@
      also
         requires n == 0;
         ensures \result == 1;
-     public static model pure long spec_factorial(int n){ 
+     public static model function pure long spec_factorial(int n){ 
         if (n == 0) {
             return 1; 
         } else {
