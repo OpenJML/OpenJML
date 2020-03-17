a4781af4b0b22f75abcc2e6fbd7107c405f14fe0
davidcok <davidcok@github.com>
2020-03-17 16:03:20 -0400

    Checkpoint

--- OpenJMLTest/test/esc2DTranspose/TransposeMatrix.java
+++ OpenJMLTest/test/esc2DTranspose/TransposeMatrix.java
@@ -1,11 +1,11 @@
      
     public class TransposeMatrix
     {
-        //@ requires matrix.length > 0;
-        //@ requires matrix[0].length > 0;
-       //@ requires (\forall int k; 0 <= k && k < matrix.length; matrix[k] != null);
-       //@ requires (\forall int k; 0 <= k && k < matrix.length; matrix[0].length == matrix[k].length);
-       //@ ensures (\forall int i, j; i >= 0 && j >= 0 && i < matrix.length && j < matrix[0].length; matrix[i][j] == \result[j][i]);
+       //@ requires matrix.length > 0;
+       //@ requires matrix[0].length > 0;
+       //@ requires (\forall int k; 0 <= k < matrix.length; matrix[k] != null);
+       //@ requires (\forall int k; 0 <= k < matrix.length; matrix[0].length == matrix[k].length);
+       //@ ensures (\forall int i, j; 0 <= i < matrix.length && 0 <= j < matrix[0].length; matrix[i][j] == \result[j][i]);
        //@ ensures matrix.length == \result[0].length;
        //@ ensures matrix[0].length == \result.length;	
        public int[][] transposeMat1(int[][] matrix)
@@ -16,16 +16,16 @@
           n = matrix[0].length;
      
           int[][] transpose = new int[n][m];
-          //@ assume \forall int i; 0 <= i < n; transpose[i] != null && transpose[i].length == m;
-          //@ assume \forall int e; 0<=e<n; \forall int k; 0 <= k < n; (e != k ==> transpose[e] != transpose[k]);
-          //@ assume \forall int e; 0<=e<n; (\forall int k; 0 <= k < m; transpose[e] != matrix[k]);
+          //@ assert \forall int i; 0 <= i < n; transpose[i] != null && transpose[i].length == m;
+          //@ assert \forall int e; 0<=e<n; \forall int k; 0 <= k < n; (e != k ==> transpose[e] != transpose[k]);
+          //@ assert \forall int e; 0<=e<n; (\forall int k; 0 <= k < m; transpose[e] != matrix[k]);
 
-          //@ maintaining 0 <= c && c <= n;
+          //@ maintaining 0 <= c <= n;
           //@ maintaining \forall int i; 0<=i<c; (\forall int j; 0 <= j < m ; transpose[i][j] == matrix[j][i]);
           //@ decreases n - c;
           for (int c = 0; c < n; c++){
-              //@ maintaining 0 <= d && d <= m;
-              //@ maintaining (\forall int j; 0 <= j && j < d; transpose[c][j] == matrix[j][c]);
+              //@ maintaining 0 <= d <= m;
+              //@ maintaining (\forall int j; 0 <= j < d; transpose[c][j] == matrix[j][c]);
               //@ decreases m - d;
               for (int d = 0; d < m; d++) {
                   transpose[c][d] = matrix[d][c];
@@ -39,30 +39,29 @@
        //@ requires (\forall int k; 0 <= k && k < matrix.length; matrix[0].length == matrix[k].length);
        //@ ensures matrix.length == \result[0].length;
        //@ ensures matrix[0].length == \result.length;  
+       //@ ensures (\forall int i, j; 0 <= i < matrix.length && 0 <= j < matrix[0].length; matrix[i][j] == \result[j][i]);
        public int[][] transposeMat2(int[][] matrix)
        {
           int m, n;
 
           m = matrix.length;
           n = matrix[0].length;
-          //@ assume n > 0;
-          //@ assume m > 0;
      
           int[][] transpose = new int[n][m];
-          //@ assume n > 0 ==> transpose[0] != null;
-          //@ assume n > 0 ==> transpose[0].length == n;
-          //@ assume \forall int i; 0 <= i < n; transpose[i] != null && transpose[i].length == m;
-          //@ assume \forall int e; 0<=e<n; \forall int k; 0 <= k < n; (e != k ==> transpose[e] != transpose[k]);
+          //@ assert n > 0 ==> transpose[0] != null;
+          //@ assert n > 0 ==> transpose[0].length == n;
+          //@ assert \forall int i; 0 <= i < n; transpose[i] != null && transpose[i].length == m;
+          //@ assert \forall int e; 0<=e<n; \forall int k; 0 <= k < n; (e != k ==> transpose[e] != transpose[k]);
 
-              //@ maintaining 0 <= d && d <= n;
-              //@ decreases n - d;
-              for (int d = 0; d < n; d++) {
-                  //@ maintaining 0 <= c && c <= m;
-                  //@ decreases m - c;
-                  for (int c = 0; c < m; c++){
-                  // @ assert transpose.length == n; 
-                  // @ assert 0 <= d < n;
-                  transpose[d][c] = 42;  
+          //@ maintaining 0 <= d && d <= n;
+          //@ maintaining (\forall int j, i; 0 <= j < matrix.length && 0 <= i < d; transpose[i][j] == matrix[j][i]);
+          //@ decreases n - d;
+          for (int d = 0; d < n; d++) {
+              //@ maintaining 0 <= c && c <= m;
+              //@ maintaining (\forall int j; 0 <= j < c; transpose[d][j] == matrix[j][d]);
+              //@ decreases m - c;
+              for (int c = 0; c < m; c++){
+                  transpose[d][c] = matrix[c][d];  
               }
           }
           return transpose;
