dc066b0d10aa5fb6d97545bb11870227cdbb6fe0
davidcok <davidcok@github.com>
2020-05-25 08:57:51 -0400

    Imrpvoement in parsing - initializer and static_initializer

--- OpenJML/src/org/jmlspecs/openjml/ext/MethodSimpleClauseExtensions.java
+++ OpenJML/src/org/jmlspecs/openjml/ext/MethodSimpleClauseExtensions.java
@@ -19,6 +19,16 @@ import com.sun.tools.javac.util.ListBuffer;
 
 public class MethodSimpleClauseExtensions extends JmlExtension.MethodClause {
     
+    public static final String normalBehaviorID = "normal_behavior";
+    public static final String exceptionalBehaviorID = "exceptional;_behavior";
+    public static final String behaviorID = "behavior";
+    public static final String feasibleBehaviorID = "feasible_behavior";
+    public static final String abruptBehaviorID = "abrupt_behavior";
+    public static final String normalExampleID = "normal_example";
+    public static final String exceptionalExampleID = "exceptional_example";
+    public static final String forExampleID = "for_example";
+    public static final String exampleID = "example";
+    public static final String impliesThatID = "implies_that";
     public static final String specGroupStartID = "{|";
     public static final String specGroupEndID = "|}";
     public static final String alsoID = "also";
@@ -30,6 +40,16 @@ public class MethodSimpleClauseExtensions extends JmlExtension.MethodClause {
     public static final IJmlClauseKind specGroupEndClause = new MethodClauseType(specGroupEndID);
     
     public static final IJmlClauseKind modelprogramClause = new MethodClauseType(modelprogramID);
+    public static final IJmlClauseKind normalBehaviorClause = new MethodClauseType(normalBehaviorID);
+    public static final IJmlClauseKind exceptionalBehaviorClause = new MethodClauseType(exceptionalBehaviorID);
+    public static final IJmlClauseKind behaviorClause = new MethodClauseType(behaviorID);
+    public static final IJmlClauseKind feasibleBehaviorClause = new MethodClauseType(feasibleBehaviorID);
+    public static final IJmlClauseKind abruptBehaviorClause = new MethodClauseType(abruptBehaviorID);
+    public static final IJmlClauseKind normalExampleClause = new MethodClauseType(normalExampleID);
+    public static final IJmlClauseKind exceptionalExampleClause = new MethodClauseType(exceptionalExampleID);
+    public static final IJmlClauseKind forExampleClause = new MethodClauseType(forExampleID);
+    public static final IJmlClauseKind exampleClause = new MethodClauseType(exampleID);
+    public static final IJmlClauseKind impliesThatClause = new MethodClauseType(impliesThatID);
     
     public static final IJmlClauseKind alsoClause = new MethodClauseType(alsoID);
     public static final IJmlClauseKind elseClause = new MethodClauseType(elseID);
@@ -41,7 +61,10 @@ public class MethodSimpleClauseExtensions extends JmlExtension.MethodClause {
     @Override
     public IJmlClauseKind[]  clauseTypesA() { return clauseTypes(); }
     public static IJmlClauseKind[]  clauseTypes() { return new IJmlClauseKind[]{
-            specGroupStartClause, specGroupEndClause, modelprogramClause, alsoClause, elseClause}; }
+            specGroupStartClause, specGroupEndClause, modelprogramClause, alsoClause, elseClause,
+//            normalBehaviorClause, exceptionalBehaviorClause, behaviorClause, feasibleBehaviorClause, abruptBehaviorClause,
+//            normalExampleClause, exceptionalExampleClause, forExampleClause, exampleClause, impliesThatClause, 
+            }; }
     
     public static class MethodClauseType extends IJmlClauseKind.MethodClause {
         public MethodClauseType(String keyword) { super(keyword); }
