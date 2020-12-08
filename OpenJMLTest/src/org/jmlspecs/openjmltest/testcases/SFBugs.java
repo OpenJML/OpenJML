0c9ae45acbd0b1d5b8f5c1d2b4c870da1a41a527
davidcok <davidcok@github.com>
2020-12-07 21:43:06 -0500

    #683: Adding test (with fixed specs) for issue #683.

--- OpenJMLTest/src/org/jmlspecs/openjmltest/testcases/SFBugs.java
+++ OpenJMLTest/src/org/jmlspecs/openjmltest/testcases/SFBugs.java
@@ -1009,6 +1009,12 @@ public class SFBugs extends EscBase {
     }
     
     @Test
+    public void gitbug683() {
+        expectedExit = 0;
+        helpTCG();
+    }
+    
+    @Test
     public void gitbug684() {
         expectedExit = 0;
         helpTCG();
