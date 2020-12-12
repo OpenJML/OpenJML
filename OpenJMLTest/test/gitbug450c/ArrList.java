a9eb538a01b6e56175bf189d578ad8fc5c939a1e
davidcok <davidcok@github.com>
2020-12-12 16:37:59 -0500

    Regression fixes

--- OpenJMLTest/test/gitbug450c/ArrList.java
+++ OpenJMLTest/test/gitbug450c/ArrList.java
@@ -5,11 +5,10 @@ import java.util.ArrayList;
 public class ArrList {
     public ArrayList<Integer> theList;
 
-    // Use default assignable, else use assignable theList, theList.*;
     //@ ensures theList != null;
     //@ ensures theList.size() == 1;
     // @ ensures theList.indexOf(a) == 0; // FIXME - at this time, the spec of ArrayList (and List) is not adequate to prove this assertion
-    //@ pure
+    //@ pure // constructor
     public ArrList(int a) {
         theList = new ArrayList<Integer>();
         theList.add(a);
