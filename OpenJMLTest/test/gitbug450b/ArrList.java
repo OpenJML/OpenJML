4718304374ba1c9df2a295956359787bc2ff3540
davidcok <davidcok@github.com>
2021-03-21 11:11:56 -0400

    More tests fixed

--- OpenJMLTest/test/gitbug450b/ArrList.java
+++ OpenJMLTest/test/gitbug450b/ArrList.java
@@ -13,27 +13,3 @@ public class ArrList {
 //        theList.add(a);
     }
 
-=======
-// This test case corresponds to Git issue #450
-
-import java.util.ArrayList;
-
-public class ArrList {
-    public ArrayList<Integer> theList;
-
-    // Use default assignable, else use assignable theList, theList.*;
-    //@ ensures theList != null;
-    //@ ensures theList.size() == 1;
-    // @ ensures theList.indexOf(a) == 0; // FIXME - at this time, the spec of ArrayList (and List) is not adequate to prove this assertion
-    public ArrList(int a) {
-        theList = new ArrayList<Integer>();
-        theList.add(a);
-    }
-
-    //@ assignable \nothing;
-    public static void main(String[] args) {
-        ArrList al = new ArrList(1);
-        // @ assert al.theList.content.owner == al.theList; // FIXME - with -minQuant, this assertion cannot be proved, though it can with -no-minQuant
-        System.out.println(al.theList.get(0));
-    }
-}
