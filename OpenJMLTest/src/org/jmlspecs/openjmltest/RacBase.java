7427dc56b77d1a314c523fabde1eef3055672b7a
davidcok <davidcok@github.com>
2021-04-07 23:15:20 -0400

    Slight fix to tests

--- OpenJMLTest/src/org/jmlspecs/openjmltest/RacBase.java
+++ OpenJMLTest/src/org/jmlspecs/openjmltest/RacBase.java
@@ -87,7 +87,7 @@ public abstract class RacBase extends JmlTestCase {
         // Use the default specs path for tests
         testspecpath = testspecpath1;
         // Define a new collector that filters out the notes
-        collector = new FilteredDiagnosticCollector<JavaFileObject>(false);
+        collector = new FilteredDiagnosticCollector<JavaFileObject>(false,false);
         super.setUp();
         
         // Setup the options
