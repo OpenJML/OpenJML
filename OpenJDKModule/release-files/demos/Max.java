ff10f95140c5d5f3dd34e82ed074f259f0456dcd
davidcok <davidcok@github.com>
2021-05-20 15:50:44 -0400

    Release files and updated build.yml

---
+++ OpenJDKModule/release-files/demos/Max.java
@@ -0,0 +1,9 @@
+public class Max {
+
+  //@ ensures \result >= i && \result >= j && \result >= k;
+  //@ ensures \result == i || \result == j || \result == k;
+  public static int max(int i, int j, int k) {
+    int t = i > j ? i : j;
+    return t > k ? t : k;
+  }
+}
