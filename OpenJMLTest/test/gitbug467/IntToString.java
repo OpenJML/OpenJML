69fa644f6156be3ff105d8959fae01a0c7b9860d
davidcok <davidcok@github.com>
2021-08-21 09:14:11 -0400

    #467, #402

--- OpenJMLTest/test/gitbug467/IntToString.java
+++ OpenJMLTest/test/gitbug467/IntToString.java
@@ -1,6 +1,14 @@
 public class IntToString {
-  public static String intToString(int x) {
-	    return x + "";
-	    //return String.valueOf(x) + ""; // OK
-  }
+	  public static String intToStringA(int x) {
+		    return x + "";
+	  }
+	  public static String intToStringB(int x) {
+		    return Integer.toString(x);
+	  }
+	  public static String intToStringC(int x) {
+		    return Integer.toString(x,10);
+	  }
+	  public static String intToStringD(int x) {
+		    return String.valueOf(x) + "";
+	  }
 }
