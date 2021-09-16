1e653ddf3ddb807af1565b5faec77c62471b01c4
davidcok <davidcok@github.com>
2021-09-16 09:02:50 -0400

    Adding artifacts for version 0.8.56

--- OpenJML/scripts/release-branches.sh
+++ OpenJML/scripts/release-branches.sh
@@ -29,7 +29,7 @@ if [ -z "$REL" ]; then
     echo "Please give the desired release number as the one argument"
     exit 1
 fi
-pushd ../../../*UpdateSite*
+pushd ../../../OpenJML-UpdateSite
 git add -u
 git commit -m "Adding artifacts for version $REL"
 git push
