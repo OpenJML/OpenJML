bf1d7d26ce54b9af45bea6da6085d8e2d6265a10
davidcok <davidcok@github.com>
2021-10-24 19:36:12 -0400

    Major rework of esc tests

--- OpenJDKModule/src/jdk.internal.vm.ci/share/classes/module-info.java
+++ OpenJDKModule/src/jdk.internal.vm.ci/share/classes/module-info.java
@@ -25,7 +25,7 @@
 
 module jdk.internal.vm.ci {
     exports jdk.vm.ci.services to
-        jdk.internal.vm.compiler,
+    jdk.internal.vm.compiler,
         jdk.internal.vm.compiler.management;
     exports jdk.vm.ci.runtime to
         jdk.internal.vm.compiler,
@@ -33,6 +33,7 @@ module jdk.internal.vm.ci {
     exports jdk.vm.ci.meta to jdk.internal.vm.compiler;
     exports jdk.vm.ci.code to jdk.internal.vm.compiler;
     exports jdk.vm.ci.hotspot to jdk.internal.vm.compiler;
+    exports jdk.vm.ci.amd64 to org.grallvm.compiler.asm.amd64; // OPENJML added for Windows build
 
     uses jdk.vm.ci.services.JVMCIServiceLocator;
     uses jdk.vm.ci.hotspot.HotSpotJVMCIBackendFactory;
