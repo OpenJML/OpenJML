03937a12cc0f80dffb5eaa86d70f1d2ff2c8c0ce
davidcok <davidcok@github.com>
2022-02-12 11:37:12 -0500

    Refactoring: more name changes

--- OpenJDKModule/src/jdk.compiler/share/classes/org/jmlspecs/openjml/JmlSpecs.java
+++ OpenJDKModule/src/jdk.compiler/share/classes/org/jmlspecs/openjml/JmlSpecs.java
@@ -845,7 +845,7 @@ public class JmlSpecs {
     	    boolean ok = JmlEnter.instance(context).requestSpecs(classSym);
             //if (!ok) System.out.println("REQUEST TO GET SPECS ONLY QUEUED: " + classSym);
     	}
-    	var ts = specsTypes.get(classSym);
+    	var ts = get(classSym);
     	if (ts == null) {
     		ts = new TypeSpecs(classSym, null, (JmlModifiers)JmlTree.Maker.instance(context).Modifiers(classSym.flags()), null);
             utils.note(true,"      inserting default class specs for " + classSym.flatname);
@@ -870,9 +870,9 @@ public class JmlSpecs {
     }
     
     public void getAttrSpecs(Symbol s) {
-    	if (s instanceof ClassSymbol) getAttrSpecs((ClassSymbol)s);
-    	if (s instanceof MethodSymbol) getAttrSpecs((MethodSymbol)s);
-    	if (s instanceof VarSymbol) getAttrSpecs((VarSymbol)s);
+    	if (s instanceof ClassSymbol cs) getAttrSpecs(cs);
+    	if (s instanceof MethodSymbol ms) getAttrSpecs(ms);
+    	if (s instanceof VarSymbol vs) getAttrSpecs(vs);
     }
     
 //    /** Retrieves the specifications for a given type, providing and registering
@@ -974,7 +974,7 @@ public class JmlSpecs {
     	if (status(m).less(SpecsStatus.SPECS_ATTR)) {
     		attr.attrSpecs(m, null);
     	}
-        return specsMethods.get(m);
+        return get(m);
     }
     
     /** Returns loaded specs: modifiers are present; specification cases may be present, 
@@ -1006,7 +1006,7 @@ public class JmlSpecs {
 //        		specsMethods.put(m,ms);
 //        	}
     	}
-    	var ms = specsMethods.get(m);
+    	var ms = get(m);
 //        if (ms == null) System.out.println("Null specs returned from getLoadedSpecs (no default) for " + m.owner + " " + m + " " + status(m) + " " + m.hashCode());
         return ms;
     }
@@ -1327,23 +1327,23 @@ public class JmlSpecs {
      * @return the specs of the field, or null if none are present
      */
     //@ nullable
-    public FieldSpecs getAttrSpecs(VarSymbol m) {
-    	if (!(m.owner instanceof ClassSymbol c)) return null;
+    public FieldSpecs getAttrSpecs(VarSymbol v) {
+    	if (!(v.owner instanceof ClassSymbol c)) return null;
     	getLoadedSpecs(c);
 //    	if (c == null) System.out.println("Unexpected difference - field " + m + " " + m.owner + " " + m.enclClass());
 //    	if (c == null) Utils.dumpStack();
-    	if (status(m).less(SpecsStatus.SPECS_ATTR)) attr.attrSpecs(m);
+    	if (status(v).less(SpecsStatus.SPECS_ATTR)) attr.attrSpecs(v);
 //        TypeSpecs t = specsmap.get(c);
 //        return t == null ? null : t.fields.get(m);
-    	return specsFields.get(m);
+    	return get(v);
     }
     
     /** Returns precisely what is in the current specs data base -- may be null */
     //@ nullable
-    public FieldSpecs get(VarSymbol m) {
+    public FieldSpecs get(VarSymbol v) {
 //        TypeSpecs t = specsmap.get(m.owner);
 //        return t == null ? null : t.fields.get(m);
-    	return specsFields.get(m);
+    	return specsFields.get(v);
     }
     
    /** Retrieves the specs for a given field
