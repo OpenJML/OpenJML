b8e61491a443329de3a299434a0080b074a39b24
davidcok <davidcok@github.com>
2022-03-04 20:07:23 -0500

    Removing the cached copy of tutorial from github

--- OpenJDKModule/release-files/tutorial/T_WellDefined2.java
+++
@@ -1,7 +0,0 @@
-// openjml -esc T_WellDefined2.java
-public class T_WellDefined2 {
-  int f;
-  public void example(/*@ non_null */ T_WellDefined2 o) {
-    //@ assert o.f == o.f;
-  }
-}
--- OpenJDKModule/release-files/tutorial/T_ensures3.java
+++
@@ -1,8 +0,0 @@
-// openjml -esc T_ensures3.java
-public class T_ensures3 {
-  //@ requires a.length > 0;
-  //@ ensures \result == a[0];
-  public int fist(int[] a) {
-    return a[0];
-  }
-}
