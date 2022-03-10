905c0f49c4afda7062a26be87b520485095e8dc0
davidcok <davidcok@github.com>
2022-03-09 22:47:36 -0500

    Progress on linked2/Linked.java -- working version except remove, with some lemma asserts needed

--- OpenJMLTest/test/linked2/Linked.java
+++ OpenJMLTest/test/linked2/Linked.java
@@ -9,6 +9,8 @@ public class Linked {
     //@ model public seq<W> values(); // sequence of values after the current Link, not including the current Link
     
     //@ public normal_behavior
+    
+    //@   reads next, next.nextFields; // FIXME - does not work as nextFields
     //@   ensures \result == ((next == null) ? (\bigint)0 : 1 + next.size());
     //@   ensures \result >= 0;
     //@ pure helper
@@ -16,11 +18,11 @@ public class Linked {
 
     // @ public invariant values().size == size();
     
-    // @ public model \locset nextFields;
-    // @ public model \locset valueFields;
+    //@ public model JMLDataGroup nextFields;
+    // @ public model JMLDataGroup valueFields;
     
     //@ nullable
-    public Linked next;// @ in nextFields; maps next.nextFields \into nextFields;
+    public Linked next;//@ in nextFields; maps next.nextFields \into nextFields;
     //@ nullable
     public W value; // @ in valueFields; maps next.valueFields \into valueFields
     
@@ -44,32 +46,24 @@ public class Linked {
     }
     
     //@ public normal_behavior
-    //@   old \bigint oldsize = (\lbl C this.size());
+    //@   old \bigint oldsize = this.size();
     //@   old seq<W> oldvalues = this.values();
     //@ assignable next;
     //@ ensures \fresh(this.next);
     //@ ensures this.next.value == t;
     //@ ensures this.next.next == \old(this.next);
-    //@ ensures (\lbl A this.size()) == (\lbl B oldsize) + 1;
+    //@ ensures this.size() == oldsize + 1;
     // @ ensures this.values().equals(oldvalues.prepend(t));
     public void push(W t) {
-        //@ assume next != null;
         //@ ghost \bigint n = this.size();
-        //@ ghost \bigint nn = this.next.size();
-        //@ show this.size(), n;
-        // @ assert n == nn + 1;
         Linked v = new Linked(t, next);
-        //@ assert v.next == this.next;
-        // @ assert nn == this.next.size();
-        // @ assert nn == v.next.size();
-        //@ assert v.size() == n;
-        // @ assert n == this.size();
+        // @ assert v.next == this.next;
+        // @ assert v.size() == n;
         // @ assert this != v;
-        // @ assert this.next != v;
+        // @ assert this != v.next;
         this.next = v;
-        //@ assume v.size() == n;
-        //@ show this.size();
-        //@ assert this.size() == 1 + n;
+        //@ assert v.size() == n;  // FIXME - this is needed as a lemma, presumably bcause the implicit axiom about size doe snto work for the postcondition alone
+        // @ assert this.size() == 1 + n;
         // @ assert this.size() == 1 + v.size();
     }
     
@@ -77,7 +71,7 @@ public class Linked {
     //@   requires this != this.next;
     //@   requires next != null;
     //@   old \bigint oldsize = this.size();
-    //@   old seq<W> oldvalues = this.values();
+    // @   old seq<W> oldvalues = this.values();
     //@   assignable next; // size, values;
     //@   ensures next == \old(next.next);
     //@   ensures this.size() == oldsize - 1;
@@ -88,20 +82,14 @@ public class Linked {
         //@ ghost var nnsize = this.next.next == null ? (\bigint)-1 : this.next.next.size();
         //@ assert osize > 0;
         //@ assert osize == nsize + 1;
-        if (this.next.next == null) {
-            //@ assert osize == 1;
-        } else {
-            //@ assert nsize == this.next.next.size() + 1;
-            //@ assert nsize == nnsize + 1;
-        }
         this.next = this.next.next;
         if (this.next == null) {
-            //@ assert osize == 1;
-            //@ assert this.next == null;
+            // @ assert osize == 1;
+            // @ assert this.next == null;
             //@ assert this.size() == 0;
         } else {
-            //@ assert nnsize == this.next.size();
-            //@ assert nsize == this.next.size() + 1;
+            // @ assert nnsize == this.next.size();
+            // @ assert nsize == this.next.size() + 1;
             //@ assert this.size() == this.next.size() + 1;
         }
         //@ assert this.size() == osize - 1;
@@ -110,17 +98,21 @@ public class Linked {
     //@ public normal_behavior
     //@   requires 0 <= n < this.size();
     //@   old \bigint oldsize = this.size();
-    //@   old seq<W> oldvalues = this.values();
-    // @   assignable size, values;
-    // @   ensures next == \old(next.next);
+    // @   old seq<W> oldvalues = this.values();
+    //@   assignable next, nextFields;
     //@   ensures this.size() == oldsize - 1;
     // @   ensures this.values().equals(oldvalues.tail(1));
     public void remove(int n) {
         //@ assert this.next != null;
+        //@ ghost \bigint nn = this.next.size();
+        //@ assert this.size() - 1 == nn;
         if (n == 0) {
             this.next = this.next.next;
+            //@ assert this.size() == nn;
         } else {
             this.next.remove(n-1);
+            //@ assert this.size() == nn;
         }
+        //@ assert this.size() == nn;
     }
 }
