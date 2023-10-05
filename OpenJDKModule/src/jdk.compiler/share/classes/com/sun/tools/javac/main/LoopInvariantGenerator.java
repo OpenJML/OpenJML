c0ecb7c156bae1b2e7a43caa538f1686a196b0d4
Andrea Hernandez <andrea.hern@knights.ucf.edu>
2023-10-05 15:54:37 +0000

    Created list of JmlVariableDecls, JCFieldAccesses, JCLiterals, and JCIdents. List still needs to be deduped and sorted.

--- OpenJDKModule/src/jdk.compiler/share/classes/com/sun/tools/javac/main/LoopInvariantGenerator.java
+++ OpenJDKModule/src/jdk.compiler/share/classes/com/sun/tools/javac/main/LoopInvariantGenerator.java
@@ -13,10 +13,15 @@ import com.sun.tools.javac.comp.AttrContext;
 import com.sun.tools.javac.comp.Env;
 import com.sun.tools.javac.comp.JmlAttr;
 import com.sun.tools.javac.parser.JmlParser;
+import com.sun.tools.javac.tree.JCTree.JCIdent;
+import com.sun.tools.javac.tree.JCTree.JCLiteral;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.JCTree.*;
+import com.sun.source.tree.Tree;
 import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.tree.TreeScanner;
+
+import java.util.ArrayList;
 
 class LoopAssertionFinder extends JmlTreeScanner {
 
@@ -54,10 +59,16 @@ class LoopAssertionFinder extends JmlTreeScanner {
             // System.out.println(tree.expression);
         }
         super.visitJmlStatementExpr(tree);
-    }
+    } 
 }
 
 class AssertionReader extends TreeScanner implements IJmlVisitor {
+    public ArrayList<Tree> possible_vars;
+
+    public AssertionReader() {
+        possible_vars = new ArrayList<Tree>();
+    }
+
     @Override
     public void scan(JCTree tree) {
         if (tree != null) {
@@ -90,11 +101,28 @@ class AssertionReader extends TreeScanner implements IJmlVisitor {
         String nameString = tree.name.toString();
         if ((selectedString.equals("arr") || selectedString.equals("a")) &&
                 (nameString.equals("length") || nameString.equals("size"))) {
+            possible_vars.add(tree);
             System.out.println("Possible array length expression: " + tree);
         }
 
         super.visitSelect(tree);
     }
+
+    @Override
+    public void visitLiteral(JCLiteral tree) {
+        possible_vars.add(tree);
+    }
+
+    @Override
+    public void visitIdent(JCIdent tree) {
+        possible_vars.add(tree);
+    }
+
+    @Override
+    public void visitJmlVariableDecl(JmlVariableDecl tree) {
+        System.out.printf("Var Def Tree=%s\n", tree);
+        possible_vars.add(tree);
+    }          
 }
 
 // class used to make IJmlClauseKind objects for loop invariants
@@ -145,6 +173,7 @@ public class LoopInvariantGenerator {
         }
 
         assertionReader.scan(lAssertionFinder.detectedAssertion.expression);
+        System.out.println("Possible Variables: " + assertionReader.possible_vars);
 
         // System.out.println(lAssertionFinder.detectedForLoop);
         // System.out.println(lAssertionFinder.detectedWhileLoop);
