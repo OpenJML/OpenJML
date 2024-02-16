2d6f491f67f179abb93e6aba47ced0ddf37ee500
davidcok <davidcok@github.com>
2024-02-16 15:38:36 -0500

    Implemented checks for completeness and disjointness of behaviors

---
+++ OpenJML21/src/jdk.compiler/share/classes/org/jmlspecs/openjml/ext/MethodExprListClauseExtensions.java
@@ -0,0 +1,59 @@
+package org.jmlspecs.openjml.ext;
+
+import org.jmlspecs.openjml.IJmlClauseKind;
+import org.jmlspecs.openjml.JmlExtension;
+import org.jmlspecs.openjml.JmlTree.JmlMethodClauseInvariants;
+
+import com.sun.tools.javac.code.Type;
+import com.sun.tools.javac.comp.AttrContext;
+import com.sun.tools.javac.comp.Env;
+import com.sun.tools.javac.comp.JmlAttr;
+import com.sun.tools.javac.parser.JmlParser;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.JCTree.JCExpression;
+import com.sun.tools.javac.tree.JCTree.JCModifiers;
+
+public class MethodExprListClauseExtensions extends JmlExtension {
+    
+    public static final String invariantsID = "invariants";
+        
+    public static final IJmlClauseKind invariantsClauseKind = new IJmlClauseKind.MethodSpecClauseKind(invariantsID) {
+        public boolean oldNoLabelAllowed() { return false; }
+        public boolean preOrOldWithLabelAllowed() { return false; }
+
+        @Override
+        public 
+        JmlMethodClauseInvariants parse(JCModifiers mods, String keyword, IJmlClauseKind clauseType, JmlParser parser) {
+            init(parser);
+            if (mods != null) {
+                error(mods, "jml.message", "A " + keyword + " clause may not have modifiers");
+                return null;
+            }
+            
+            int pp = parser.pos();
+            int pe = parser.endPos();
+            
+            parser.nextToken();
+            var n = parser.parseOptionalName();
+            var exprs = parser.parseExpressionList();
+            JmlMethodClauseInvariants cl = parser.maker().at(pp).JmlMethodClauseInvariants(exprs);
+            System.out.println("INVS EXPRS " + exprs);
+            System.out.println("INVS CLAUSE " + cl);
+            wrapup(cl, clauseType, true, true);
+            if (cl != null) cl.name = n;
+            System.out.println("INVS EXPRS " + exprs);
+            System.out.println("INVS CLAUSE " + cl);
+            return cl;
+        }
+        
+        @Override
+        public Type typecheck(JmlAttr attr, JCTree tree, Env<AttrContext> env) {
+            // TODO Auto-generated method stub
+            JmlMethodClauseInvariants cl = (JmlMethodClauseInvariants)tree;
+            for (var e: cl.expressions) attr.attribTree(cl,  env, null);
+            return null;
+        }
+        
+};
+    
+}
