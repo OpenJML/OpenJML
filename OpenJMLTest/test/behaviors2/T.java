0f948a9a64b28900db14ea8a8feb6f8317273740
davidcok <davidcok@github.com>
2024-02-16 19:31:39 -0500

    More tests and completion of implementation of completeness testing

--- OpenJMLTest/test/behaviors2/T.java
+++ OpenJMLTest/test/behaviors2/T.java
@@ -1,18 +1,39 @@
-public class T {
+public class T extends U {
 
   //@ requires i < 0;
   //@ also
   //@ requires i > 0;
-  //@ behaviors complete;
+  //@ behaviors complete; // ERROR
   public void m(int i) {}
 
   //@ requires i <= 0;
   //@ also
   //@ requires i >= 0;
   //@ also
-  //@ requires i == 1;
-  //@ behaviors complete;
-  //@ behaviors disjoint;
+  //@ requires i == 1; 
+  //@ behaviors complete; // OK
+  //@ behaviors disjoint; // ERROR - 2 combinations
   public void mcomplete(int i) {}
+
+  //@ also
+  //@ requires i < 2;
+  //@ also
+  //@ requires i < 3;
+  //@ behaviors local_disjoint; // ERROR - just one pair
+  public void mlocal(int i) {}
+
+  //@ also
+  //@ requires i < -3;
+  //@ behaviors local_complete; // ERROR
+  public void mlocal2(int i) {}
+}
+
+class U {
+
+  //@ requires i > 0;
+  public void mlocal(int i) {}
+
+  //@ requires i > 0;
+  public void mlocal2(int i) {}
 
 }
