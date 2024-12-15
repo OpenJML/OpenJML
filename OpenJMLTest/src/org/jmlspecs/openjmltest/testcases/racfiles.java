c6e8d5c4be41b99493084dc8f4ed94157d723dbc
davidcok <davidcok@github.com>
2024-12-15 07:46:26 -0500

    WIP on range syntax

--- OpenJMLTest/src/org/jmlspecs/openjmltest/testcases/racfiles.java
+++ OpenJMLTest/src/org/jmlspecs/openjmltest/testcases/racfiles.java
@@ -551,6 +551,11 @@ public class racfiles extends RacBase {
     }
     
     @Test
+    public void rangeTest1() {
+        helpTCF("test/rangeTest","test/rangeTest","R");
+    }
+    
+    @Test
     public void record1() {
         helpTCF("test/record1","test/record1","RR");
     }
