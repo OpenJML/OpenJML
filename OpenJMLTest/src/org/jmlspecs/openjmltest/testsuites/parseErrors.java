4da914bb61db16c3d8fd13b5a816631f1fc52a88
davidcok <davidcok@github.com>
2025-05-25 15:59:08 -0400

    Cleanup and additional harness tests for parser and scanner tests

--- OpenJMLTest/src/org/jmlspecs/openjmltest/testsuites/parseErrors.java
+++ OpenJMLTest/src/org/jmlspecs/openjmltest/testsuites/parseErrors.java
@@ -3,6 +3,7 @@ package org.jmlspecs.openjmltest.testsuites;
 import org.jmlspecs.openjmltest.ParseBase;
 import org.junit.Ignore;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 @org.junit.FixMethodOrder(org.junit.runners.MethodSorters.NAME_ASCENDING)
 public class parseErrors extends ParseBase {
@@ -14,51 +15,51 @@ public class parseErrors extends ParseBase {
     }
 
     @Test public void badTry() {
-        checkCompilationUnitErrors("class A { public A() { try{}} }"
+        checkParseErrors("class A { public A() { try{}} }"
                 ,"/TEST.java:1: error: 'try' without 'catch', 'finally' or resource declarations", 24, 23, 23, 23 // FIXME - Better would be end=26
 
                 );
     }
 
     @Test public void orphanCatch() {
-        checkCompilationUnitErrors("class A { public A() { catch(Exception e) {}} }"
+        checkParseErrors("class A { public A() { catch(Exception e) {}} }"
                 ,"/TEST.java:1: error: 'catch' without 'try'", 24, 23, 23, 23 // FIXME - Beeeter would be end=28
                 );
     }
 
     @Test public void orphanFinally() {
-        checkCompilationUnitErrors("class A { public A() { finally {}} }"
+        checkParseErrors("class A { public A() { finally {}} }"
                 ,"/TEST.java:1: error: 'finally' without 'try'", 24, 31, 23, 23 // FIXME - start is after end
                 );
     }
 
     @Test public void orphanElse() {
-        checkCompilationUnitErrors("class A { public A() { else {}} }"
+        checkParseErrors("class A { public A() { else {}} }"
                 ,"/TEST.java:1: error: 'else' without 'if'", 24, 28, 23, 23 // FIXME - start is after end
                 );
     }
 
     @Test public void orphanCase() {
-        checkCompilationUnitErrors("class A { public A() { case 0; } }"
+        checkParseErrors("class A { public A() { case 0; } }"
                 ,"/TEST.java:1: error: orphaned case", 24, 23, 23, 23
                 ,"/TEST.java:1: error: : or -> expected", 30, 29, 29, 29
                 );
     }
 
     @Test public void orphanDefault() {
-        checkCompilationUnitErrors("class A { public A() { default: } }"
+        checkParseErrors("class A { public A() { default: } }"
                 ,"/TEST.java:1: error: orphaned default", 24, 23, 23, 23
                 );
     }
 
     // FIXME - should complain about missing 'model'
     @Test public void modelClassWithNoMods() {
-        checkCompilationUnitErrors("class A { /*@ class B {} */ }"
+        checkParseErrors("class A { /*@ class B {} */ }"
                 );
     }
 
     @Test public void modifierOnImport() {
-        checkCompilationUnitErrors("//@ @Pure import java.lang.System; \nclass A { }"
+        checkParseErrors("//@ @Pure import java.lang.System; \nclass A { }"
                 ,"/TEST.java:1: error: No modifiers are allowed on an import statement", 5, 4, 4, 15
                 ,"/TEST.java:1: error: An import statement in a JML comment must have a model modifier", 11, 10, 10, 15
                 ,"/TEST.java:1: warning: misplaced model import", 11, 10, 10, 35 // FIXME - need a clearer error message
@@ -66,19 +67,19 @@ public class parseErrors extends ParseBase {
     }
 
     @Test public void modifierOnImport2() {
-        checkCompilationUnitErrors("//@ @Model import java.lang.System; \nclass A { }"
+        checkParseErrors("//@ @Model import java.lang.System; \nclass A { }"
                 ,"/TEST.java:1: warning: misplaced model import", 12, 11, 11, 36  // FIXME - need a clearer error message
                 );
     }
 
     @Test public void modifierOnImportOK() {
-        checkCompilationUnitErrors("//@ model import java.lang.System; \nclass A { }"
+        checkParseErrors("//@ model import java.lang.System; \nclass A { }"
                 );
     }
 
     // FIXME - not sure this helps coverage
     @Test public void prematureTypeListEnd() {
-        checkCompilationUnitErrors("class A { public A() { /*@ model T<A B> t; */} }"
+        checkParseErrors("class A { public A() { /*@ model T<A B> t; */} }"
                 ,"/TEST.java:1: error: > or ',' expected", 38, 37, 37, 37
                 ,"/TEST.java:1: error: ';' expected", 39, 38, 38, 38
                 ,"/TEST.java:1: error: not a statement", 41, 40, 40, 40
@@ -90,7 +91,7 @@ public class parseErrors extends ParseBase {
 
     // FIXME - not sure this helps coverage
     @Test public void prematureTypeListEnd2() {
-        checkCompilationUnitErrors("class A { public A() { /*@ model T<A:B> t; */} }"
+        checkParseErrors("class A { public A() { /*@ model T<A:B> t; */} }"
                 ,"/TEST.java:1: error: > or ',' expected", 37, 36, 36, 36
                 ,"/TEST.java:1: error: not a statement", 39, 37, 38, 38
                 ,"/TEST.java:1: error: Expected a declaration or a JML construct inside the JML annotation here", 38, 37, 37, 37
@@ -99,7 +100,7 @@ public class parseErrors extends ParseBase {
     }
 
     @Test public void missingCase() {
-        checkCompilationUnitErrors("class A { public void m() { switch (x) { x = 0; } } }"
+        checkParseErrors("class A { public void m() { switch (x) { x = 0; } } }"
                 ,"/TEST.java:1: error: case, default, or '}' expected", 42, 41, 41, 41
                 ,"/TEST.java:1: error: case, default, or '}' expected", 44
                 ,"/TEST.java:1: error: case, default, or '}' expected", 46
@@ -108,32 +109,32 @@ public class parseErrors extends ParseBase {
     }
 
     @Test public void badForInit() {
-        checkCompilationUnitErrors("class A { public void m() { for (int : x) {}  } }"
+        checkParseErrors("class A { public void m() { for (int : x) {}  } }"
                 ,"/TEST.java:1: error: bad initializer for for-loop", 34, 33, 33, 33
                 );
     }
 
     @Test public void emptySpecCase() {
         addOptions("--lang=jml");
-        checkCompilationUnitErrors("class A { /*@ normal_behavior */ public void m() {  {}  } }"
+        checkParseErrors("class A { /*@ normal_behavior */ public void m() {  {}  } }"
                 ,"/TEST.java:1: error: The specification case near here is empty, which is not permitted", 15, 14, 14, 14
                 );
     }
     
     @Test public void badJmlType() {
-        checkCompilationUnitErrors("class A { public void m(/*@[ A @*/ A b) {  {}  } }"
+        checkParseErrors("class A { public void m(/*@[ A @*/ A b) {  {}  } }"
                 ,"/TEST.java:1: error: ']' expected", 31, 30, 30, 30
                 );
     }
 
     @Test public void badJmlType2() {
-        checkCompilationUnitErrors("class A { public void m(/*@[ A ] x @*/ A b) {  {}  } }"
+        checkParseErrors("class A { public void m(/*@[ A ] x @*/ A b) {  {}  } }"
                 ,"/TEST.java:1: error: Incorrectly formed or terminated JML construct near here", 34, 33, 33, 33
                 );
     }
 
     @Test public void badJmlType3() {
-        checkCompilationUnitErrors("class A { public void m(/*@ /*@[ A ] x @*/ @*/ A b) {  {}  } }"
+        checkParseErrors("class A { public void m(/*@ /*@[ A ] x @*/ @*/ A b) {  {}  } }"
                 ,"/TEST.java:1: error: Block comments may not be embedded inside JML block comments", 29, 28, 28, 28
                 ,"/TEST.java:1: error: Incorrectly formed or terminated JML construct near here", 38, 37, 37, 37
                 ,"/TEST.java:1: error: illegal start of type", 46, 45, 45, 45
@@ -142,14 +143,14 @@ public class parseErrors extends ParseBase {
 
     // FIXME - review these
     @Test public void modsOnImpliesThat() {
-        checkCompilationUnitErrors("class A { /*@ requires true; also public implies_that requires true; */ public void m() { } }"
+        checkParseErrors("class A { /*@ requires true; also public implies_that requires true; */ public void m() { } }"
                 ,"/TEST.java:1: error: No modifiers are allowed prior to a lightweight specification case", 35, 34, 34, 34
                 //,"/TEST.java:1: warning: No modifiers are allowed prior to a implies_that token", 35, 34, 34, 34
                 );
     }
 
     @Test public void modsOnForExample() {
-        checkCompilationUnitErrors("class A { /*@ requires true; also public for_example requires true; */ public void m() { } }"
+        checkParseErrors("class A { /*@ requires true; also public for_example requires true; */ public void m() { } }"
                 ,"/TEST.java:1: error: No modifiers are allowed prior to a lightweight specification case", 35, 34, 34, 34
                 //,"/TEST.java:1: warning: No modifiers are allowed prior to a for_example token", 35, 34, 34, 34
                 );
@@ -157,7 +158,7 @@ public class parseErrors extends ParseBase {
 
     @Ignore // FIXME - implement feasible behavior
     @Test public void modsOnFeasibleBehavior() {
-        checkCompilationUnitErrors("class A { /*@ requires true; also public feasible_behavior requires true; */ public void m() { } }"
+        checkParseErrors("class A { /*@ requires true; also public feasible_behavior requires true; */ public void m() { } }"
                 ,"/TEST.java:1: warning: No modifiers are allowed prior to a feasible_behavior token", 42, 41, 41, 57
                 );
     }
@@ -165,8 +166,177 @@ public class parseErrors extends ParseBase {
     @Ignore // FIXME - don't seem to be able to add a Java option to enable preview features
     @Test public void stringTemplate() {
         addOptions("--source","21","--enable-preview");
-        checkCompilationUnitErrors("class A { String s = STR.\"My \\{x} template\"; }"
+        checkParseErrors("class A { String s = STR.\"My \\{x} template\"; }"
                 );
+    }
+    
+    // Test harness tests -- checking that test failures are properly reported
+    
+    @Test
+    public void harness1() {
+        noExtraPrinting = true;
+        try {
+            checkParseErrors("public c A {}");
+        } catch (AssertionError a) {
+            assertEquals("Intentional failure issued wrong message",
+                    "More errors observed (1) than expected (0)",
+                    a.getMessage());
+        }
+    }
+    
+    @Test
+    public void harness2() {
+        noExtraPrinting = true;
+        try {
+            checkParseErrors("public c A {}"
+                ,"/TEST.java:1: error: class, interface, enum, or record expected"
+                );
+        } catch (AssertionError a) {
+            assertEquals("Intentional failure issued wrong message", 
+                    "No positions given for message 0", 
+                    a.getMessage());
+        }
+    }
+    
+    @Test
+    public void harness3() {
+        noExtraPrinting = true;
+        try {
+            checkParseErrors("public c A {}"
+                ,"/TEST.java:1: error: class, interface, enum, or record expected", 8
+                ,"ZZZ", 8
+                );
+        } catch (AssertionError a) {
+            assertEquals("Intentional failure issued wrong message", 
+                    "Fewer errors observed (1) than expected. First extra: ZZZ", 
+                    a.getMessage());
+        }
+    }    
+
+    @Test
+    public void harness4() {
+        noExtraPrinting = true;
+        try {
+            checkParseErrors("public c A {}"
+                ,"ZZZ", 8
+                );
+        } catch (AssertionError a) {
+            assertEquals("Intentional failure issued wrong message", 
+                    "Message 0 mismatch expected:<[ZZZ]> but was:<[/TEST.java:1: error: class, interface, enum, or record expected]>", 
+                    a.getMessage());
+        }
+    }
+
+    @Test
+    public void harness5() {
+        noExtraPrinting = true;
+        try {
+            checkParseErrors("public c A {}"
+                ,"/TEST.java:1: error: class, interface, enum, or record expected", 99
+                );
+        } catch (AssertionError a) {
+            assertEquals("Intentional failure issued wrong message", 
+                    "Column for message 0 expected:<99> but was:<8>", 
+                    a.getMessage());
+        }
+    }
+
+    @Test
+    public void harness6() {
+        noExtraPrinting = true;
+        try {
+            checkParseErrors("public c A {}"
+                ,"/TEST.java:1: error: class, interface, enum, or record expected", 8, 7, 7, 7
+                );
+        } catch (AssertionError a) {
+            assertEquals("Intentional failure issued wrong message", 
+                    "Start for message 0 expected:<7> but was:<0>", 
+                    a.getMessage());
+        }
+    }
+
+    @Test
+    public void harness7() {
+        noExtraPrinting = true;
+        try {
+            checkParseErrors("public c A {}"
+                ,"/TEST.java:1: error: class, interface, enum, or record expected", 8, 0, -10, -10
+                );
+        } catch (AssertionError a) {
+            assertEquals("Intentional failure issued wrong message", 
+                    "Position for message 0 expected:<-10> but was:<7>", 
+                    a.getMessage());
+        }
+    }
+
+    @Test
+    public void harness8() {
+        noExtraPrinting = true;
+        try {
+            checkParseErrors("public c A {}"
+                ,"/TEST.java:1: error: class, interface, enum, or record expected", 8, 0, 7, -10
+                );
+        } catch (AssertionError a) {
+            assertEquals("Intentional failure issued wrong message", 
+                    "End for message 0 expected:<-10> but was:<7>", 
+                    a.getMessage());
+        }
+    }
+
+    @Test
+    public void harness9() {
+        noExtraPrinting = true;
+        try {
+            checkParseErrors("public c A {}"
+                ,"/TEST.java:1: error: class, interface, enum, or record expected", 8, 0
+                );
+        } catch (AssertionError a) {
+            assertEquals("Intentional failure issued wrong message", 
+                    "Expected 0 or 3 position values after the column value", 
+                    a.getMessage());
+        }
+    }
+
+    @Test
+    public void harness10() {
+        noExtraPrinting = true;
+        try {
+            checkParseErrors("public c A {}"
+                ,"/TEST.java:1: error: class, interface, enum, or record expected", 8, 0, 7
+                );
+        } catch (AssertionError a) {
+            assertEquals("Intentional failure issued wrong message", 
+                    "Expected 0 or 3 position values after the column value", 
+                    a.getMessage());
+        }
+    }
+
+    @Test
+    public void harness11() {
+        noExtraPrinting = true;
+        try {
+            checkParseErrors("public c A {}"
+                ,"/TEST.java:1: error: class, interface, enum, or record expected", 8, 0, 7, 7, 7
+                );
+        } catch (AssertionError a) {
+            assertEquals("Intentional failure issued wrong message", 
+                    "Fewer errors observed (1) than expected. First extra: 7", 
+                    a.getMessage());
+        }
+    }
+
+    @Test
+    public void harness12() {
+        noExtraPrinting = true;
+        try {
+            checkParseErrors("public c A {}"
+                ,8, 0, 7, 7, 7
+                );
+        } catch (AssertionError a) {
+            assertEquals("Intentional failure issued wrong message", 
+                    "Expected a message string instead of 8", 
+                    a.getMessage());
+        }
     }
 
 
