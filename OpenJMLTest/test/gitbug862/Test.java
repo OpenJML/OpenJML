a44cbfeb7379076d938679eac5ae3eb1d7ca22d4
davidcok <davidcok@github.com>
2025-06-09 22:56:22 -0400

    Fixed issue #862 -- corrected array creation to use resolved element type instead of formal element type (which might be a type parameter)

--- OpenJMLTest/test/gitbug862/Test.java
+++ OpenJMLTest/test/gitbug862/Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        new java.util.ArrayList<Integer>().equals(java.util.Arrays.asList(1));
+        java.util.List<Integer> x = java.util.Arrays.<Integer>asList((Integer)1);
+        //new java.util.ArrayList<Integer>().equals(java.util.Arrays.asList(1));
     }
 }
 
