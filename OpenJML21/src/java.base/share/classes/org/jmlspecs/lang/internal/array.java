82ed116147101531fc195597442e7c8b52aea2e8
davidcok <davidcok@github.com>
2025-07-06 06:43:50 -0400

    Cleaning up \seq and \array

--- OpenJML21/src/java.base/share/classes/org/jmlspecs/lang/internal/array.java
+++ OpenJML21/src/java.base/share/classes/org/jmlspecs/lang/internal/array.java
@@ -6,14 +6,17 @@ import org.jmlspecs.lang.IJmlIntArrayLike;
 //@ immutable no_state 
 public class array<T> implements IJmlPrimitiveType, IJmlIntArrayLike {
     
-    private T[] value;
+    private final T[] value;
+    public final bigint length;
     
-    private array() { value = null; }
-    private array(T[] v) { value = v; }
+    private array() { value = null; length = bigint.zero; }
+    private array(T[] v) { value = v; length = bigint.of(value.length); }
     
     public static <TT> array<TT> empty() { return new array<TT>(); }
 
-    public static <TT> array<TT> of(TT[] data) { return new array<TT>(data); }
+    @SafeVarargs
+    @SuppressWarnings("unchecked")
+    public static <TT> array<TT> of(TT ... data) { return new array<TT>(data); }
 
     public array<T> copy() { 
         if (value == null) return array.<T>empty();
@@ -21,10 +24,12 @@ public class array<T> implements IJmlPrimitiveType, IJmlIntArrayLike {
     }
 
     public T get(bigint i) {
+        if (value == null || i.lt(bigint.zero) || i.ge(length)) throw new java.lang.ArrayIndexOutOfBoundsException();
         return value[i.intValue()];
     }
     
     public array<T> put(bigint i, T v) {
+        if (value == null || i.lt(bigint.zero) || i.ge(length)) throw new java.lang.ArrayIndexOutOfBoundsException();
         var c = copy();
         c.value[i.intValue()] = v;
         return c;
@@ -45,4 +50,13 @@ public class array<T> implements IJmlPrimitiveType, IJmlIntArrayLike {
     }
     
     public T[] value() { return value; }
+    
+    public int hashCode() {
+        return value == null ? 0 : value.hashCode();
+    }
+    
+    public boolean equals(Object o) {
+        throw new UnsupportedOperationException("\\array<T>.equals not supported; use == or .eq instead");
+    }
+
 }
