59e62499f2e125b860eba16ca3d57b7e7a683856
davidcok <davidcok@github.com>
2025-10-22 16:44:18 -0400

    Resolved #761

--- OpenJMLTest/src/org/jmlspecs/openjmltest/testsuites/escfiles2.java
+++ OpenJMLTest/src/org/jmlspecs/openjmltest/testsuites/escfiles2.java
@@ -77,6 +77,11 @@ public class escfiles2 extends EscBaseFiles {
     }
     
     @Test
+    public void gitbug761() {
+        helpTCG("--check");
+    }
+    
+    @Test
     public void gitbug762() {
         helpTCG("--check");
     }
