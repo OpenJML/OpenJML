8b166f0af759774843afe880ad501e631fb68e60
davidcok <davidcok@github.com>
2025-11-13 13:20:20 -0500

    Resolving #894

--- OpenJMLTest/src/org/jmlspecs/openjmltest/testsuites/escfiles2.java
+++ OpenJMLTest/src/org/jmlspecs/openjmltest/testsuites/escfiles2.java
@@ -166,6 +166,11 @@ public class escfiles2 extends EscBaseFiles {
     }
     
     @Test
+    public void gitbug894() {
+        helpTCG("--check-feasibility=none");
+    }
+    
+    @Test
     public void gitbug895() {
         helpTCG();
     }
