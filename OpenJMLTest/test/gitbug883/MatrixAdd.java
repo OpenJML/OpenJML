6121ded239e70b41280c48f029d200faa859efb5
davidcok <davidcok@github.com>
2025-12-06 10:34:52 -0500

    Fixed #883 by adding implicit assumptions about non-freshness of components of 2D arrays

--- OpenJMLTest/test/gitbug883/MatrixAdd.java
+++ OpenJMLTest/test/gitbug883/MatrixAdd.java
@@ -10,22 +10,22 @@ public class MatrixAdd {
     //@ ensures \forall int i; 0 <= i < 2; \forall int j; 0 <= j < 2; \result[i][j] == a[i][j] + b[i][j]; 
     public int[][] add(int[][] a, int[][] b) {
         int[][] c = new int[2][2];
+        //@ assert \forall int i; 0 <= i < c.length; \forall int j; 0 <= j < a.length; c[i] != a[j];
+        //@ assert \forall int i; 0 <= i < c.length; \forall int j; 0 <= j < b.length; c[i] != b[j];
 
         //@ maintaining 0 <= i <= 2;
-        //@ maintaining \forall int k; 0 <= k < 2; c[k] == \old(c[k],\LoopInit); // FIXME - perhaps c[*][*] being writable allows a c[*] to be written
         //@ maintaining \forall int m; 0 <= m < 2; \forall int n; 0 <= n < 2; Integer.MIN_VALUE <= a[m][n] + b[m][n] <= Integer.MAX_VALUE;
         //@ maintaining \forall int m; 0 <= m < i; \forall int n; 0 <= n < 2; c[m][n] == a[m][n] + b[m][n];
-        //@ loop_writes i, c[*][*]; // c[i];
+        //@ loop_writes i, c[*][*];
         //@ decreases 2 - i;
         for (int i = 0; i < 2; i++) {
-            //@ maintaining \forall int m; 0 <= m < i; \forall int n; 0 <= n < 2; c[m][n] == a[m][n] + b[m][n];
             //@ maintaining 0 <= j <= 2;
+            //@ maintaining \forall int m; 0 <= m < i; \forall int n; 0 <= n < 2; c[m][n] == a[m][n] + b[m][n];
             //@ maintaining \forall int m; 0 <= m < 2; \forall int n; 0 <= n < 2; Integer.MIN_VALUE <= a[m][n] + b[m][n] <= Integer.MAX_VALUE;
             //@ maintaining \forall int k; 0 <= k < j; c[i][k] == a[i][k] + b[i][k];
-            //@ loop_writes j, c[i][*]; // c[i][j];
+            //@ loop_writes j, c[i][*];
             //@ decreases 2 - j;
             for (int j = 0; j < 2; j++) {
-                //@ assert Integer.MIN_VALUE <= a[i][j] + b[i][j] <= Integer.MAX_VALUE;
                 c[i][j] = a[i][j] + b[i][j];
             }
         }
