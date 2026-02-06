9c8090e8a0700c231e4b09f2883536cdfc3e97fe
davidcok <davidcok@github.com>
2026-02-05 23:48:04 -0500

    Noop

--- OpenJML21/src/jdk.compiler/share/classes/org/jmlspecs/openjml/esc/MethodProverSMT.java
+++ OpenJML21/src/jdk.compiler/share/classes/org/jmlspecs/openjml/esc/MethodProverSMT.java
@@ -1226,6 +1226,12 @@ public class MethodProverSMT {
                                                 String nmmm = nmm + "_" + pdetail3;
                                                 Boolean vvv = findPreValue(nmmm,info);
                                                 //log.note("jml.message",nmmm + " " + vvv);
+//                                                if (vvv == null) {
+//                                                    var vvvv = getValue(nmmm,info.smt,solver,false);
+//                                                    if (vvvv == null) break;
+//                                                    vvv = "true".equals(vvvv.toString()) ? true : "false".equals(vvvv.toString()) ? false : null;
+//                                                    if (vvv == null) break;
+//                                                }
                                                 if (vvv == null) break;
                                                 if (!vvv) {
                                                     JCTree s = findPreExpr(nmmm);
@@ -1585,6 +1591,12 @@ public class MethodProverSMT {
                                             String nmmm = nmm + "_" + pdetail3;
                                             Boolean vvv = findPreValue(nmmm,info);
                                             //log.note("jml.message",nmmm + " " + vvv);
+//                                            if (vvv == null) {
+//                                                var vvvv = getValue(nmmm,info.smt,solver,false);
+//                                                if (vvvv == null) break;
+//                                                vvv = "true".equals(vvvv.toString()) ? true : "false".equals(vvvv.toString()) ? false : null;
+//                                                if (vvv == null) break;
+//                                            }
                                             if (vvv == null) break;
                                             if (!vvv) {
                                                 JCExpression s = findPreExpr(nmmm);
