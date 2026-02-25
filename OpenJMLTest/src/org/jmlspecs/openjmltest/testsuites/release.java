97a12078e972fa354ad0c1dbdd75f347299708e8
davidcok <davidcok@github.com>
2026-02-24 21:35:43 -0500

    Cleaning up release and compiler testsuites

--- OpenJMLTest/src/org/jmlspecs/openjmltest/testsuites/release.java
+++ OpenJMLTest/src/org/jmlspecs/openjmltest/testsuites/release.java
@@ -2,9 +2,6 @@ package org.jmlspecs.openjmltest.testsuites;
 
 import org.jmlspecs.openjmltest.JmlTestSuite;
 
-//import java.io.ByteArrayOutputStream;
-//import java.io.PrintStream;
-
 import static org.junit.Assert.*;
 import org.junit.*;
 import org.junit.rules.TestName;
@@ -26,7 +23,6 @@ import org.junit.rules.TestName;
 public class release extends JmlTestSuite {
 
     public static final String relsrc = "releaseTests/src";
-    public static final String src = "test/compiler/";
 
     @Rule
     public TestName name = new TestName();
@@ -44,7 +40,6 @@ public class release extends JmlTestSuite {
 
     @After
     public void tearDown() {
-        // Do this just in case the test fails without having reset the streams
     }
 
     /** This is a helper method that runs the compiler on the given set of
@@ -111,7 +106,7 @@ public class release extends JmlTestSuite {
         }
     }
 
-    // The remaining tests are replicates of those executed by 'make release-test'
+    // These tests are replicates of those executed by 'make release-test'
     // FIXME - check the version
     // FIXME - testOK2, testOK3, testJmlBad2
     // FIXME - test RAC-OK, SIMPLE, etc.
