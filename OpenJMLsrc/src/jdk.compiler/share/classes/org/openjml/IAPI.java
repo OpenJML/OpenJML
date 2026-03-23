d7535b4c35f13d3c19b6c9a3274a7af3cca33c4e
davidcok <davidcok@github.com>
2026-03-22 23:03:14 -0400

    new experimental API

--- OpenJMLsrc/src/jdk.compiler/share/classes/org/openjml/IAPI.java
+++ OpenJMLsrc/src/jdk.compiler/share/classes/org/openjml/IAPI.java
@@ -27,7 +27,7 @@ public interface IAPI {
     
     public boolean isOptionSet(String key);
     public String getOption(String key);
-//    public String getSpecs(MethodSymbol methodSymbol);
+    public String getSpecs(MethodSymbol methodSymbol);
 
     
     public static final int OK = Main.Result.OK.exitCode;
